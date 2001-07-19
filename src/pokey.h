c164e9f4957985b6e661784832eecb9a55633e29
Piotr Fusik <fox@scene.pl>
2001-07-19 23:19:22 +0000

    added definitions from pokeysnd

--- src/pokey.h
+++ src/pokey.h
@@ -34,6 +34,8 @@
 #define _IRQST 0x0e
 #define _SKSTAT 0x0f
 
+#define _POKEY2 0x10			/* offset to second pokey chip (STEREO expansion) */
+
 extern UBYTE KBCODE;
 extern UBYTE IRQST;
 extern UBYTE IRQEN;
@@ -46,21 +48,16 @@ void POKEY_PutByte(UWORD addr, UBYTE byte);
 void POKEY_Initialise(int *argc, char *argv[]);
 void POKEY_Scanline(void);
 
-/* borrowed from Thomas' version */
-/* channel definitions */
-#define CHAN1       0
-#define CHAN2       1
-#define CHAN3       2
-#define CHAN4       3
-#define SAMPLE      4
+/* CONSTANT DEFINITIONS */
 
-extern UBYTE AUDF[8];    /* AUDFx (D200, D202, D204, D206) */
-extern UBYTE AUDC[8];    /* AUDCx (D201, D203, D205, D207) */
-extern UBYTE AUDCTL;     /* AUDCTL (D208) */      
-extern int DivNIRQ[4],DivNMax[4];
+/* definitions for AUDCx (D201, D203, D205, D207) */
+#define NOTPOLY5    0x80		/* selects POLY5 or direct CLOCK */
+#define POLY4       0x40		/* selects POLY4 or POLY17 */
+#define PURE        0x20		/* selects POLY4/17 or PURE tone */
+#define VOL_ONLY    0x10		/* selects VOLUME OUTPUT ONLY */
+#define VOLUME_MASK 0x0f		/* volume mask */
 
-#define DIV_64      28			/* divisor for 1.79MHz clock to 64 kHz */
-#define DIV_15      114			/* divisor for 1.79MHz clock to 15 kHz */
+/* definitions for AUDCTL (D208) */
 #define POLY9       0x80		/* selects POLY9 or POLY17 */
 #define CH1_179     0x40		/* selects 1.78979 MHz for Ch 1 */
 #define CH3_179     0x20		/* selects 1.78979 MHz for Ch 3 */
@@ -69,5 +66,42 @@ extern int DivNIRQ[4],DivNMax[4];
 #define CH1_FILTER  0x04		/* selects channel 1 high pass filter */
 #define CH2_FILTER  0x02		/* selects channel 2 high pass filter */
 #define CLOCK_15    0x01		/* selects 15.6999kHz or 63.9210kHz */
+
+/* for accuracy, the 64kHz and 15kHz clocks are exact divisions of
+   the 1.79MHz clock */
+#define DIV_64      28			/* divisor for 1.79MHz clock to 64 kHz */
+#define DIV_15      114			/* divisor for 1.79MHz clock to 15 kHz */
+
+/* the size (in entries) of the 4 polynomial tables */
+#define POLY4_SIZE  0x000f
+#define POLY5_SIZE  0x001f
+#define POLY9_SIZE  0x01ff
+
+#ifdef COMP16					/* if 16-bit compiler */
+#define POLY17_SIZE 0x00007fffL	/* reduce to 15 bits for simplicity */
+#else
+#define POLY17_SIZE 0x0001ffffL	/* else use the full 17 bits */
+#endif
+
+#define MAXPOKEYS         2		/* max number of emulated chips */
+
+/* channel/chip definitions */
+#define CHAN1       0
+#define CHAN2       1
+#define CHAN3       2
+#define CHAN4       3
+#define CHIP1       0
+#define CHIP2       4
+#define CHIP3       8
+#define CHIP4      12
+#define SAMPLE    127
+
+/* structures to hold the 9 pokey control bytes */
+extern UBYTE AUDF[4 * MAXPOKEYS];	/* AUDFx (D200, D202, D204, D206) */
+extern UBYTE AUDC[4 * MAXPOKEYS];	/* AUDCx (D201, D203, D205, D207) */
+extern UBYTE AUDCTL[MAXPOKEYS];	/* AUDCTL (D208) */
+
+extern int DivNIRQ[4],DivNMax[4];
+extern ULONG Base_mult[MAXPOKEYS];		/* selects either 64Khz or 15Khz clock mult */
 
 #endif
