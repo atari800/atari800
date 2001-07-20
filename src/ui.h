8d4b453e64547062dc62f9be79cd14cf5a6d24f7
Piotr Fusik <fox@scene.pl>
2001-07-20 20:11:04 +0000

    declared SelectCartType(), removed mach_xlxe and Ram256 (which are declared in atari.h)

--- src/ui.h
+++ src/ui.h
@@ -1,12 +1,12 @@
 #ifndef _UI_H
 #define _UI_H
-#include	"atari.h"	/* for UBYTE */
-void ui(UBYTE * screen);
+#include "atari.h"	/* for UBYTE */
+
+int SelectCartType(UBYTE *screen, int k);
+void ui(UBYTE *screen);
 
 extern int ui_is_active;
 
 extern unsigned char ascii_to_screen[128];
 
-extern int mach_xlxe;
-extern int Ram256;
 #endif
