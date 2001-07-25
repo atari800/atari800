d15faf4c0b9f77801d79967627c6e77eafebfac8
Piotr Fusik <fox@scene.pl>
2001-07-25 12:54:32 +0000

    removed unused declarations

--- src/sio.h
+++ src/sio.h
@@ -18,14 +18,6 @@
 
 #include "atari.h"
 
-
-/*
- * it seems, there are two different ATR formats with different handling for
- * DD sectors
- */
-#define SIO2PC_ATR	1
-#define OTHER_ATR	2
-
 typedef enum tagUnitStatus {
 	Off,
 	NoDisk,
@@ -37,14 +29,10 @@ extern char sio_status[256];
 extern UnitStatus drive_status[MAX_DRIVES];
 extern char sio_filename[MAX_DRIVES][FILENAME_LEN];
 
-int SIO_Mount(int diskno, const char *filename, int b_open_readonly );
+int SIO_Mount(int diskno, const char *filename, int b_open_readonly);
 void SIO_Dismount(int diskno);
 void SIO_DisableDrive(int diskno);
 void SIO(void);
-UnitStatus SIO_Drive_Status( int diskno );
-
-void SIO_SEROUT(unsigned char byte, int cmd);
-int SIO_SERIN(void);
 
 #define SIO_NoFrame         (0x00)
 #define SIO_CommandFrame    (0x01)
@@ -57,8 +45,8 @@ int SIO_SERIN(void);
 void SwitchCommandFrame(int onoff);
 void SIO_PutByte(int byte);
 int SIO_GetByte(void);
-void SIO_Initialize(void);
 void SIO_Initialise(int *argc, char *argv[]);
+void SIO_Exit(void);
 
 /* Some defines about the serial I/O timing. Currently fixed! */
 #define XMTDONE_INTERVAL 15
