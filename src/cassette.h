fb29e96356a0ca65b63d12b02d30406cc7702b7e
Piotr Fusik <fox@scene.pl>
2001-08-29 09:13:00 +0000

    Using FILENAME_MAX from stdio.h.

--- src/cassette.h
+++ src/cassette.h
@@ -1,7 +1,6 @@
-#include <stdio.h>		/* for FILE */
+#include <stdio.h>		/* for FILE and FILENAME_MAX */
 
 #include "atari.h"		/* for UBYTE */
-#include "rt-config.h"	/* for MAX_FILENAME_LEN */
 
 #define CASSETTE_DESCRIPTION_MAX 256
 
@@ -10,7 +9,7 @@ void CASSETTE_Initialise(int *argc, char *argv[]);
 int CASSETTE_CheckFile(char *filename, FILE **fp, char *description, int *last_block, int *isCAS);
 int CASSETTE_Insert(char *filename);
 void CASSETTE_Remove(void);
-extern char cassette_filename[MAX_FILENAME_LEN];
+extern char cassette_filename[FILENAME_MAX];
 extern char cassette_description[CASSETTE_DESCRIPTION_MAX];
 
 extern int cassette_current_block;
