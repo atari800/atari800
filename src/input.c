a28ed5df876fabd68aeab4a574cdf8502f1507c5
Piotr Fusik <fox@scene.pl>
2001-09-27 09:33:50 +0000

    added INPUT_Initialise, PORT_input, POT_input

--- src/input.c
+++ src/input.c
@@ -4,6 +4,7 @@
 #include "gtia.h"
 #include "input.h"
 #include "memory.h"
+#include "pia.h"
 #include "platform.h"
 #include "pokey.h"
 
@@ -16,6 +17,51 @@ int joy_autofire[4] = {AUTOFIRE_OFF, AUTOFIRE_OFF, AUTOFIRE_OFF, AUTOFIRE_OFF};
 
 int mouse_mode = MOUSE_OFF;
 int mouse_port = 0;
+int mouse_delta_x = 0;
+int mouse_delta_y = 0;
+int mouse_buttons = 0;
+int mouse_pen_ofs_h = 0;
+int mouse_pen_ofs_v = 0;
+
+void INPUT_Initialise(int *argc, char *argv[])
+{
+	int i;
+	int j;
+
+	for (i = j = 1; i < *argc; i++) {
+		if (strcmp(argv[i], "-mouse") == 0) {
+			char *mode = argv[++i];
+			if (strcmp(mode, "off") == 0)
+				mouse_mode = MOUSE_OFF;
+			else if (strcmp(mode, "pad") == 0)
+				mouse_mode = MOUSE_PAD;
+			else if (strcmp(mode, "touch") == 0)
+				mouse_mode = MOUSE_TOUCH;
+			else if (strcmp(mode, "koala") == 0)
+				mouse_mode = MOUSE_KOALA;
+			else if (strcmp(mode, "pen") == 0)
+				mouse_mode = MOUSE_PEN;
+			else if (strcmp(mode, "amiga") == 0)
+				mouse_mode = MOUSE_AMIGA;
+			else if (strcmp(mode, "st") == 0)
+				mouse_mode = MOUSE_ST;
+			else if (strcmp(mode, "joy") == 0)
+				mouse_mode = MOUSE_JOY;
+		}
+		else if (strcmp(argv[i], "-mouseport") == 0) {
+			mouse_port = argv[++i][0] - '0';
+			if (mouse_port < 0 || mouse_port > 3) {
+				Aprint("Invalid mouse port, using 0");
+				mouse_port = 0;
+			}
+		}
+		else {
+			argv[j++] = argv[i];
+		}
+	}
+
+	*argc = j;
+}
 
 void INPUT_Frame(void)
 {
@@ -60,11 +106,18 @@ void INPUT_Frame(void)
 	}
 
 	/* handle joysticks */
-	for(i = 0; i < 4; i++) {
+	PORT_input[0] = Atari_PORT(0);
+	PORT_input[1] = Atari_PORT(1);
+	for (i = 0; i < 4; i++) {
 		TRIG[i] = Atari_TRIG(i);
 		if ((joy_autofire[i] == AUTOFIRE_FIRE && !TRIG[i]) || (joy_autofire[i] == AUTOFIRE_CONT))
 			TRIG[i] = (nframes & 2) ? 1 : 0;
 	}
+
+	/* handle paddles */
+	for (i = 0; i < 8; i++)
+		POT_input[i] = Atari_POT(i);
+
 	if (machine_type == MACHINE_XLXE) {
 		TRIG[2] = 1;
 		TRIG[3] = cartA0BF_enabled;
