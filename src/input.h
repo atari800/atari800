5d4e11b92a0700ba96bb7a651a6c0301a0b6f59e
Piotr Fusik <fox@scene.pl>
2001-09-27 22:32:09 +0000

    mouse_speed, INPUT_CenterMousePointer, INPUT_DrawMousePointer

--- src/input.h
+++ src/input.h
@@ -262,6 +262,7 @@ extern int mouse_port;		/* Atari port, to which the emulated device is attached 
 extern int mouse_delta_x;	/* x motion since last frame */
 extern int mouse_delta_y;	/* y motion since last frame */
 extern int mouse_buttons;	/* buttons (b0=1: first button pressed, b1=1: 2nd pressed, etc. */
+extern int mouse_speed;		/* how fast the mouse pointer moves */
 extern int mouse_pen_ofs_h;	/* light pen/gun horizontal offset (for calibration) */
 extern int mouse_pen_ofs_v;	/* light pen/gun vertical offset (for calibration) */
 
@@ -269,3 +270,5 @@ extern int mouse_pen_ofs_v;	/* light pen/gun vertical offset (for calibration) *
 
 void INPUT_Initialise(int *argc, char *argv[]);
 void INPUT_Frame(void);
+void INPUT_CenterMousePointer(void);
+void INPUT_DrawMousePointer(void);
