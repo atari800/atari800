35e249e6b0e051d370767bfb260c7686e35ee089
Piotr Fusik <fox@scene.pl>
2001-10-01 17:15:23 +0000

    added a few macros (dGetWord, etc.)

--- src/memory-d.h
+++ src/memory-d.h
@@ -1,15 +1,36 @@
-#include <string.h>	/* memset */
+#include <string.h>	/* memcpy, memset */
 
 #include "config.h"
 
-#ifdef PAGED_MEM
-#define dGetByte(x) GetByte(x)
-UBYTE GetByte(UWORD addr);
-#define dPutByte(x,y) PutByte(x,y)
-int PutByte(UWORD addr, UBYTE byte);
-#else
-#define dGetByte(x) (memory[x])
-#define dPutByte(x,y) (memory[x] = y)
+#define dGetByte(x)				(memory[x])
+#define dPutByte(x,y)			(memory[x] = y)
+
+#ifndef WORDS_BIGENDIAN
+#ifdef UNALIGNED_LONG_OK
+#define dGetWord(x)				(* (UWORD *) &memory[x])
+#define dPutWord(x,y)			(* (UWORD *) &memory[x] = (y))
+#define dGetWordAligned(x)		dGetWord(x)
+#define dPutWordAligned(x,y)	dPutWord(x,y)
+#else	/* UNALIGNED_LONG_OK */
+#define dGetWord(x)				(memory[x] | (memory[(x) + 1] << 8))
+#define dPutWord(x,y)			(memory[x] = (UBYTE) (y), memory[x + 1] = (UBYTE) ((y) >> 8))
+/* faster versions of dGetWord and dPutWord for even addresses */
+/* TODO: guarantee that memory is UWORD-aligned and use UWORD access */
+#define dGetWordAligned(x)		dGetWord(x)
+#define dPutWordAligned(x,y)	dPutWord(x,y)
+#endif	/* UNALIGNED_LONG_OK */
+#else	/* WORDS_BIGENDIAN */
+/* can't do any word optimizations for big endian machines */
+#define dGetWord(x)				(memory[x] | (memory[(x) + 1] << 8))
+#define dPutWord(x,y)			(memory[x] = (UBYTE) (y), memory[x + 1] = (UBYTE) ((y) >> 8))
+#define dGetWordAligned(x)		dGetWord(x)
+#define dPutWordAligned(x,y)	dPutWord(x,y)
+#endif	/* WORDS_BIGENDIAN */
+
+#define dCopyFromMem(from,to,size)		memcpy(to, memory + (from), size)
+#define dCopyToMem(from,to,size)		memcpy(memory + (to), from, size)
+#define dFillMem(addr1,value,length)	memset(memory + (addr1), value, length)
+
 extern UBYTE memory[65536];
 extern UBYTE attrib[65536];
 #define	GetByte(addr)		((attrib[addr] == HARDWARE) ? Atari800_GetByte(addr) : memory[addr])
@@ -20,8 +41,6 @@ extern UBYTE attrib[65536];
 #define SetRAM(addr1,addr2) memset(attrib+addr1,RAM,addr2-addr1+1)
 #define SetROM(addr1,addr2) memset(attrib+addr1,ROM,addr2-addr1+1)
 #define SetHARDWARE(addr1,addr2) memset(attrib+addr1,HARDWARE,addr2-addr1+1)
-#endif
-#define Poke(x,y) (dPutByte((x),(y)))
 
 extern int have_basic;
 extern int cartA0BF_enabled;
