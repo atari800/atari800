962040b91d80fb01e63719fba815b04f7fa20253
Piotr Fusik <fox@scene.pl>
2001-10-01 17:10:12 +0000

    added CRASH_MENU externs

--- src/ui.h
+++ src/ui.h
@@ -9,4 +9,10 @@ extern int ui_is_active;
 
 extern unsigned char ascii_to_screen[128];
 
+#ifdef CRASH_MENU
+extern int crash_code;
+extern UWORD crash_address;
+extern UWORD crash_afterCIM;
+#endif
+
 #endif
