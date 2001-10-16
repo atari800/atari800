085a639c384caaa9486f959e72169593e0160ab9
Krzysztof Nikiel <knik@poczta.onet.pl>
2001-10-16 17:11:27 +0000

    keyboard autorepeat rate changed

--- src/ui_basic.c
+++ src/ui_basic.c
@@ -101,7 +101,7 @@ unsigned char ascii_to_screen[128] =
 
 
 #define KB_DELAY		20
-#define KB_AUTOREPEAT		4
+#define KB_AUTOREPEAT		3
 
 int GetKeyPress(UBYTE * screen)
 {
@@ -990,6 +990,9 @@ void BasicUIInit()
 
 /*
 $Log$
+Revision 1.4  2001/10/16 17:11:27  knik
+keyboard autorepeat rate changed
+
 Revision 1.3  2001/10/11 17:27:22  knik
 added atari_sync() call in keyboard loop--keyboard is sampled
 at reasonable rate
