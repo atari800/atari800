4f386416f9f8ce0c8f5bbfc7348bc8516f0eff02
Petr Stehlík <petr@pstehlik.cz>
2001-12-14 17:18:54 +0000

    version++

--- src/atari.h
+++ src/atari.h
@@ -59,7 +59,7 @@ extern int verbose;
 #define ATARI_WIDTH  384
 #define ATARI_HEIGHT 240
 
-#define ATARI_TITLE  "Atari 800 Emulator, Version 1.2.0"
+#define ATARI_TITLE  "Atari 800 Emulator, Version 1.2.1pre0"
 
 extern int xpos;
 extern int xpos_limit;
@@ -221,6 +221,9 @@ void atari_sync(void);
 
 /*
 $Log$
+Revision 1.24  2001/12/14 17:18:54  joy
+version++
+
 Revision 1.23  2001/10/29 17:58:49  fox
 changed implementation of WSYNC/VCOUNT timing
 
