7a0cb87ffcfb87a4ee4514b4a87bba8c11924fdd
Michael Borisov <borata@brain.uni-bremen.de>
2002-12-12 13:54:11 +0000

    Changed include-guard macro not to start with underscore

--- src/mzpokeysnd.h
+++ src/mzpokeysnd.h
@@ -1,5 +1,5 @@
-#ifndef _MZPOKEYSND_H
-#define _MZPOKEYSND_H
+#ifndef MZX_9385CEFB_MZPOKEYSND_H_INCLUDED
+#define MZX_9385CEFB_MZPOKEYSND_H_INCLUDED
 
 int Pokey_sound_init_mz(uint32 freq17, uint16 playback_freq, uint8 num_pokeys,
 			int flags, int quality);
