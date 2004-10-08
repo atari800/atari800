6b9e010a1e1c2c60ac15146bb8b376d8bd8d58a7
Sebastian Bauer <sba@users.sf.net>
2004-10-08 12:37:22 +0000

    Revision bump. Readme update.

--- src/amiga/Atari800_rev.h
+++ src/amiga/Atari800_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		2
-#define REVISION	1
-#define DATE		"24.9.2004"
-#define VERS		"Atari800 2.1"
-#define VSTRING		"Atari800 2.1 (24.9.2004)\r\n"
-#define VERSTAG		"\0$VER: Atari800 2.1 (24.9.2004)"
+#define REVISION	2
+#define DATE		"8.10.2004"
+#define VERS		"Atari800 2.2"
+#define VSTRING		"Atari800 2.2 (8.10.2004)\r\n"
+#define VERSTAG		"\0$VER: Atari800 2.2 (8.10.2004)"
--- src/amiga/Atari800_rev.s
+++ src/amiga/Atari800_rev.s
@@ -1,21 +1,21 @@
 VERSION = 2
-REVISION = 1
+REVISION = 2
 
 .macro DATE
-.ascii "24.9.2004"
+.ascii "8.10.2004"
 .endm
 
 .macro VERS
-.ascii "Atari800 2.1"
+.ascii "Atari800 2.2"
 .endm
 
 .macro VSTRING
-.ascii "Atari800 2.1 (24.9.2004)"
+.ascii "Atari800 2.2 (8.10.2004)"
 .byte 13,10,0
 .endm
 
 .macro VERSTAG
 .byte 0
-.ascii "$VER: Atari800 2.1 (24.9.2004)"
+.ascii "$VER: Atari800 2.2 (8.10.2004)"
 .byte 0
 .endm
