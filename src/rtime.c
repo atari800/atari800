bdc1a2d8c81b5f08862aeedc5398ab67d9221e75
Piotr Fusik <fox@scene.pl>
2005-08-06 18:27:38 +0000

    optimized RTIME_GetByte

--- src/rtime.c
+++ src/rtime.c
@@ -2,7 +2,7 @@
  * rtime.c - Emulate ICD R-Time 8 cartridge
  *
  * Copyright (C) 2000 Jason Duerstock
- * Copyright (C) 2000-2003 Atari800 development team (see DOC/CREDITS)
+ * Copyright (C) 2000-2005 Atari800 development team (see DOC/CREDITS)
  *
  * This file is part of the Atari800 emulator project which emulates
  * the Atari 400, 800, 800XL, 130XE, and 5200 8-bit computers.
@@ -35,8 +35,8 @@ static int rtime_state = 0;
 				/* 0 = waiting for register # */
 				/* 1 = got register #, waiting for hi nybble */
 				/* 2 = got hi nybble, waiting for lo nybble */
-static int rtime_tmp = 0; 
-static int rtime_tmp2 = 0; 
+static int rtime_tmp = 0;
+static int rtime_tmp2 = 0;
 
 static int regset[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
@@ -55,7 +55,7 @@ void RTIME_Initialise(int *argc, char *argv[])
 
 static int hex2bcd(int h)
 {
-	return(((h/10) << 4) | (h % 10));
+	return ((h / 10) << 4) | (h % 10);
 }
 
 static int gettime(int p)
@@ -68,81 +68,52 @@ static int gettime(int p)
 
 	switch (p) {
 	case 5:
-		return(hex2bcd(lt->tm_year));
+		return hex2bcd(lt->tm_year);
 	case 4:
-		return(hex2bcd(lt->tm_mon + 1)); /* Months are zero-base indexed by RTIME */
+		return hex2bcd(lt->tm_mon + 1);
 	case 3:
-		return(hex2bcd(lt->tm_mday));
+		return hex2bcd(lt->tm_mday);
 	case 2:
-		return(hex2bcd(lt->tm_hour));
+		return hex2bcd(lt->tm_hour);
 	case 1:
-		return(hex2bcd(lt->tm_min));
+		return hex2bcd(lt->tm_min);
 	case 0:
-		return(hex2bcd(lt->tm_sec));
+		return hex2bcd(lt->tm_sec);
 	case 6:
-		return(hex2bcd(((lt->tm_wday+2)%7)+1));
+		return hex2bcd(((lt->tm_wday + 2) % 7) + 1);
 	}
-	return(0);
-}
-
-static void fillarray()
-{
-	regset[0] = gettime(0); 	
-	regset[1] = gettime(1); 	
-	regset[2] = gettime(2); 	
-	regset[3] = gettime(3); 	
-	regset[4] = gettime(4); 	
-	regset[5] = gettime(5); 	
-	regset[6] = gettime(6); 	
+	return 0;
 }
 
 UBYTE RTIME_GetByte(void)
 {
-	fillarray();
-	switch(rtime_state) {
+	switch (rtime_state) {
 	case 0:
 		/* Aprint("pretending rtime not busy, returning 0"); */
-		return(0);
+		return 0;
 	case 1:
-		if (rtime_tmp > 5)
-		Aprint("returning %d as hi nybble of register %d",
-			regset[rtime_tmp] >> 4, rtime_tmp);
 		rtime_state = 2;
-		return(regset[rtime_tmp] >> 4);
+		return (rtime_tmp <= 6 ? gettime(rtime_tmp) : regset[rtime_tmp]) >> 4;
 	case 2:
-		if (rtime_tmp > 5)
-		Aprint("returning %d as lo nybble of register %d",
-			regset[rtime_tmp] & 0x0f, rtime_tmp);
 		rtime_state = 0;
-		return(regset[rtime_tmp] & 0x0f);
+		return (rtime_tmp <= 6 ? gettime(rtime_tmp) : regset[rtime_tmp]) & 0x0f;
 	}
-	return(0);
+	return 0;
 }
 
 void RTIME_PutByte(UBYTE byte)
 {
-	/* Aprint("d5xx write: 0x%x @ 0x%x", byte, addr); */
 	switch (rtime_state) {
 	case 0:
-		byte &= 0x0f;
-		if (byte > 5)
-		Aprint("setting active register to %d", byte);
-		rtime_tmp = byte;
+		rtime_tmp = byte & 0x0f;
 		rtime_state = 1;
 		break;
 	case 1:
-		if (rtime_tmp > 5)
-		Aprint("got %d as hi nybble of register %d",
-			byte, rtime_tmp);	
 		rtime_tmp2 = byte << 4;
 		rtime_state = 2;
 		break;
 	case 2:
-		if (rtime_tmp > 5)
-		Aprint("got %d as lo nybble, register %d now = %d",
-			byte, rtime_tmp, rtime_tmp2 | byte);
-		rtime_tmp2 |= byte;
-		regset[rtime_tmp] = rtime_tmp2;
+		regset[rtime_tmp] = rtime_tmp2 | (byte & 0x0f);
 		rtime_state = 0;
 		break;
 	}
