696a655443e6006979c2a7edb364cc72fb3f0ac9
Piotr Fusik <fox@scene.pl>
2005-08-15 17:23:49 +0000

    declarations for cpu.c

--- src/monitor.h
+++ src/monitor.h
@@ -1,6 +1,26 @@
 #ifndef _MONITOR_H_
 #define _MONITOR_H_
 
+#include "atari.h"
+#include "config.h"
+
 int monitor(void);
+
+#ifdef TRACE
+extern int tron;
+#endif
+
+#ifdef MONITOR_BREAK
+extern UWORD break_addr;
+extern UWORD ypos_break_addr;
+extern UBYTE break_step;
+extern UBYTE break_ret;
+extern UBYTE break_cim;
+extern UBYTE break_here;
+extern int ret_nesting;
+extern int brkhere;
+#endif
+
+unsigned int disassemble(UWORD addr1, UWORD addr2);
 
 #endif /* _MONITOR_H_ */
