8f6997a58a6bda87e162974b4fa6802ee402b439
Piotr Fusik <fox@scene.pl>
2005-08-16 23:11:36 +0000

    #include "pokeysnd.h" for uint32, uint16, uint8

--- src/mzpokeysnd.h
+++ src/mzpokeysnd.h
@@ -1,6 +1,8 @@
 #ifndef _MZPOKEYSND_H_
 #define _MZPOKEYSND_H_
 
+#include "pokeysnd.h"
+
 int Pokey_sound_init_mz(uint32 freq17, 
                         uint16 playback_freq, 
                         uint8 num_pokeys,
