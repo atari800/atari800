b2b5d896981aa83a7405f345d26ef89a8bf3be01
Piotr Fusik <fox@scene.pl>
2005-09-04 18:16:55 +0000

    Atari 5200 keys

--- src/atari_svgalib.c
+++ src/atari_svgalib.c
@@ -24,6 +24,7 @@
 
 #include "config.h"
 #include <stdio.h>
+#include <stdlib.h> /* exit() */
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
@@ -39,8 +40,7 @@
 #include "log.h"
 #include "ui.h"
 #include "screen.h"
-
-extern int refresh_rate;
+#include "rt-config.h"
 
 #ifdef SVGA_JOYMOUSE
 #include <vgamouse.h>
@@ -65,7 +65,7 @@ static int js1_centre_y;
 #endif
 
 static struct JS_DATA_TYPE js_data;
-#endif
+#endif /* LINUX_JOYSTICK */
 
 #include <linux/keyboard.h>
 #include <vgakeyboard.h>
@@ -77,39 +77,39 @@ static int kbcode = 0;
 static int kbjoy = 1;
 static UBYTE joydefs[] =
 {
-  SCANCODE_KEYPAD0,	/* fire */
-  SCANCODE_KEYPAD7,	/* up/left */
-  SCANCODE_KEYPAD8,	/* up */
-  SCANCODE_KEYPAD9,	/* up/right */
-  SCANCODE_KEYPAD4,	/* left */
-  SCANCODE_KEYPAD6,	/* right */
-  SCANCODE_KEYPAD1,	/* down/left */
-  SCANCODE_KEYPAD2,	/* down */
-  SCANCODE_KEYPAD3,	/* down/right */
+	SCANCODE_KEYPAD0,	/* fire */
+	SCANCODE_KEYPAD7,	/* up/left */
+	SCANCODE_KEYPAD8,	/* up */
+	SCANCODE_KEYPAD9,	/* up/right */
+	SCANCODE_KEYPAD4,	/* left */
+	SCANCODE_KEYPAD6,	/* right */
+	SCANCODE_KEYPAD1,	/* down/left */
+	SCANCODE_KEYPAD2,	/* down */
+	SCANCODE_KEYPAD3,	/* down/right */
 #ifdef USE_CURSORBLOCK
-  SCANCODE_CURSORBLOCKUP,
-  SCANCODE_CURSORBLOCKLEFT,
-  SCANCODE_CURSORBLOCKRIGHT,
-  SCANCODE_CURSORBLOCKDOWN,
+	SCANCODE_CURSORBLOCKUP,
+	SCANCODE_CURSORBLOCKLEFT,
+	SCANCODE_CURSORBLOCKRIGHT,
+	SCANCODE_CURSORBLOCKDOWN,
 #endif
 };
 
 static UBYTE joymask[] =
 {
-  0, 		/* not used */
-  ~1 & ~4,	/* up/left */
-  ~1, 		/* up */
-  ~1 & ~8, 	/* up/right */
-  ~4,		/* left */
-  ~8,		/* right */
-  ~2 & ~4,	/* down/left */
-  ~2,		/* down */
-  ~2 & ~8,	/* down/right */
+	0,       /* not used */
+	~1 & ~4, /* up/left */
+	~1,      /* up */
+	~1 & ~8, /* up/right */
+	~4,      /* left */
+	~8,      /* right */
+	~2 & ~4, /* down/left */
+	~2,      /* down */
+	~2 & ~8, /* down/right */
 #ifdef USE_CURSORBLOCK
-  ~1, 		/* up */
-  ~4,		/* left */
-  ~8,		/* right */
-  ~2,		/* down */
+	~1,      /* up */
+	~4,      /* left */
+	~8,      /* right */
+	~2,      /* down */
 #endif
 };
 
@@ -143,283 +143,328 @@ static int scrn_ptr_inc = ATARI_WIDTH;
 /****************************************************************************/
 static void kbhandler(int code, int state)
 {
-  if (code < 0 || code >= NR_KEYS)
-    {
-      fprintf(stderr, "keyboard code out of range (0x%x).\n", code);
-      return;
-    }
+	if (code < 0 || code >= NR_KEYS) {
+		fprintf(stderr, "keyboard code out of range (0x%x).\n", code);
+		return;
+	}
 #if 0
-  printf("code: %x(%x) - %s\n", code, state, keynames[code]);
+	printf("code: %x(%x) - %s\n", code, state, keynames[code]);
 #endif
-  if ((code == SCANCODE_BREAK)
-       || (code == SCANCODE_BREAK_ALTERNATIVE))
-    {
-      if (state)
-	pause_hit = 1;
-      return;
-    }
-  kbhits[kbcode = code] = state;
-  if (!state)
-    kbcode |= 0x100;
+	if (code == SCANCODE_BREAK
+	 || code == SCANCODE_BREAK_ALTERNATIVE) {
+		if (state)
+			pause_hit = 1;
+		return;
+	}
+	kbhits[kbcode = code] = state;
+	if (!state)
+		kbcode |= 0x100;
 }
 
 static void initkb(void)
 {
-  int i;
-  if (keyboard_init())
-  {
-    fprintf(stderr,"unable to initialize keyboard\n");
-    exit(1);
-  }
-  for (i = 0; i < sizeof(kbhits) / sizeof(kbhits[0]); i++)
-    kbhits[i] = 0;
-  keyboard_seteventhandler(kbhandler);
+	int i;
+	if (keyboard_init()) {
+		fprintf(stderr,"unable to initialize keyboard\n");
+		exit(1);
+	}
+	for (i = 0; i < sizeof(kbhits) / sizeof(kbhits[0]); i++)
+		kbhits[i] = 0;
+	keyboard_seteventhandler(kbhandler);
 }
 
 int Atari_Keyboard(void)
 {
 	int keycode;
-        int i;
+	int i;
 
-        keyboard_update();
+	keyboard_update();
 
-        if (kbjoy)
-        {
-          /* fire */
+	if (kbjoy) {
+		/* fire */
 #ifdef USE_CURSORBLOCK
-          ctrig = (kbhits[joydefs[0]] ? 0 : 1) & (kbhits[SCANCODE_LEFTCONTROL] ? 0 : 1);
+		ctrig = (kbhits[joydefs[0]] ? 0 : 1) & (kbhits[SCANCODE_LEFTCONTROL] ? 0 : 1);
 #else
-          ctrig = kbhits[joydefs[0]] ? 0 : 1;
+		ctrig = kbhits[joydefs[0]] ? 0 : 1;
 #endif
-          cstick |= 0xf;
-          for (i = 1; i < sizeof(joydefs) / sizeof(joydefs[0]); i++)
-            if (kbhits[joydefs[i]])
-              cstick &= joymask[i];
-        }
+		cstick |= 0xf;
+		for (i = 1; i < sizeof(joydefs) / sizeof(joydefs[0]); i++)
+			if (kbhits[joydefs[i]])
+				cstick &= joymask[i];
+	}
 
-        key_consol = (key_consol & ~CONSOL_NONE)
-        	| (kbhits[SCANCODE_F2] ? 0 : CONSOL_OPTION)
-        	| (kbhits[SCANCODE_F3] ? 0 : CONSOL_SELECT)
-        	| (kbhits[SCANCODE_F4] ? 0 : CONSOL_START);
+	key_shift = (kbhits[SCANCODE_LEFTSHIFT]
+	           | kbhits[SCANCODE_RIGHTSHIFT]) ? 1 : 0;
 
-        if (pause_hit)
-        {
-          pause_hit = 0;
-          return AKEY_BREAK;
-        }
+	alt_function = -1;		/* no alt function */
+	if (kbhits[0x38]) {		/* left Alt key is pressed */
+		if (kbcode == SCANCODE_R)
+			alt_function = MENU_RUN;		/* ALT+R .. Run file */
+		else if (kbcode == SCANCODE_Y)
+			alt_function = MENU_SYSTEM;		/* ALT+Y .. Select system */
+		else if (kbcode == SCANCODE_O)
+			alt_function = MENU_SOUND;		/* ALT+O .. mono/stereo sound */
+		else if (kbcode == SCANCODE_W)
+			alt_function = MENU_SOUND_RECORDING;	/* ALT+W .. record sound */
+		else if (kbcode == SCANCODE_A)
+			alt_function = MENU_ABOUT;		/* ALT+A .. About */
+		else if (kbcode == SCANCODE_S)
+			alt_function = MENU_SAVESTATE;	/* ALT+S .. Save state */
+		else if (kbcode == SCANCODE_D)
+			alt_function = MENU_DISK;		/* ALT+D .. Disk management */
+		else if (kbcode == SCANCODE_L)
+			alt_function = MENU_LOADSTATE;	/* ALT+L .. Load state */
+		else if (kbcode == SCANCODE_C)
+			alt_function = MENU_CARTRIDGE;	/* ALT+C .. Cartridge management */
+	}
+	if (alt_function != -1)
+		return AKEY_UI;
 
-        key_shift = (kbhits[SCANCODE_LEFTSHIFT]
-                   | kbhits[SCANCODE_RIGHTSHIFT]) ? 1 : 0;
+	/* need to set shift mask here to avoid conflict with PC layout */
+	keycode = (key_shift ? 0x40 : 0)
+	        | ((kbhits[SCANCODE_LEFTCONTROL]
+	          | kbhits[SCANCODE_RIGHTCONTROL]) ? 0x80 : 0);
 
-		alt_function = -1;		/* no alt function */
-		if (kbhits[0x38]) {		/* left Alt key is pressed */
-			if (kbcode == SCANCODE_R)
-				alt_function = MENU_RUN;		/* ALT+R .. Run file */
-			else if (kbcode == SCANCODE_Y)
-				alt_function = MENU_SYSTEM;		/* ALT+Y .. Select system */
-			else if (kbcode == SCANCODE_O)
-				alt_function = MENU_SOUND;		/* ALT+O .. mono/stereo sound */
-			else if (kbcode == SCANCODE_W)
-				alt_function = MENU_SOUND_RECORDING;	/* ALT+W .. record sound */
-			else if (kbcode == SCANCODE_A)
-				alt_function = MENU_ABOUT;		/* ALT+A .. About */
-			else if (kbcode == SCANCODE_S)
-				alt_function = MENU_SAVESTATE;	/* ALT+S .. Save state */
-			else if (kbcode == SCANCODE_D)
-				alt_function = MENU_DISK;		/* ALT+D .. Disk management */
-			else if (kbcode == SCANCODE_L)
-				alt_function = MENU_LOADSTATE;	/* ALT+L .. Load state */
-			else if (kbcode == SCANCODE_C)
-				alt_function = MENU_CARTRIDGE;	/* ALT+C .. Cartridge management */
+	switch (kbcode) {
+	case SCANCODE_F9:
+		return AKEY_EXIT;
+	case SCANCODE_F8:
+		kbcode = 0;
+		return Atari_Exit(1) ? AKEY_NONE : AKEY_EXIT;
+	case SCANCODE_SCROLLLOCK: /* pause */
+		while (kbhits[SCANCODE_SCROLLLOCK])
+			keyboard_update();
+		while (!kbhits[SCANCODE_SCROLLLOCK])
+			keyboard_update();
+		kbcode = 0;
+		break;
+	case SCANCODE_PRINTSCREEN:
+	case SCANCODE_F10:
+		kbcode = 0;
+		return key_shift ? AKEY_SCREENSHOT_INTERLACE : AKEY_SCREENSHOT;
+	case SCANCODE_F1:
+		return AKEY_UI;
+	case SCANCODE_F5:
+		return key_shift ? AKEY_COLDSTART : AKEY_WARMSTART;
+	case SCANCODE_F11:
+		for (i = 0; i < 4; i++) {
+			if (++joy_autofire[i] > 2)
+				joy_autofire[i] = 0;
 		}
-		if (alt_function != -1)
-			return AKEY_UI;
+		kbcode = 0;
+		break;
+	default:
+		break;
+	}
 
-        /* need to set shift mask here to avoid conflict with PC layout */
-        keycode = (key_shift ? 0x40 : 0)
-        	| ((kbhits[SCANCODE_LEFTCONTROL]
-                   | kbhits[SCANCODE_RIGHTCONTROL]) ? 0x80 : 0);
-
-        switch (kbcode)
-        {
-          case SCANCODE_F9:
-            return AKEY_EXIT;
-          case SCANCODE_F8:
-            keycode = Atari_Exit(1) ? AKEY_NONE : AKEY_EXIT;
-            kbcode = 0;
-            break;
-          case SCANCODE_SCROLLLOCK: /* pause */
-            while (kbhits[SCANCODE_SCROLLLOCK])
-              keyboard_update();
-            while (!kbhits[SCANCODE_SCROLLLOCK])
-              keyboard_update();
-            kbcode = 0;
-            break;
-          case SCANCODE_PRINTSCREEN:
-          case SCANCODE_F10:
-			keycode = key_shift ? AKEY_SCREENSHOT_INTERLACE : AKEY_SCREENSHOT;
-			kbcode = 0;
+	if (machine_type == MACHINE_5200 && !ui_is_active) {
+		switch (kbcode) {
+		case SCANCODE_F4:
+			keycode |= AKEY_5200_START;
 			break;
-          case SCANCODE_F1:
-            return AKEY_UI;
-          case SCANCODE_F5:
-            return key_shift ? AKEY_COLDSTART : AKEY_WARMSTART;
-          case SCANCODE_F6:
-            return AKEY_HELP;
-          case SCANCODE_F7:
-            return AKEY_BREAK;
-          case SCANCODE_F11:
-            for(i = 0; i < 4; i++)
-            {
-              if (++joy_autofire[i] > 2)
-                joy_autofire[i] = 0;
-            }
-            kbcode = 0;
-            break;
+		case SCANCODE_P:
+			keycode |= AKEY_5200_PAUSE;
+			break;
+		case SCANCODE_R:
+			keycode |= AKEY_5200_RESET;
+			break;
+		case SCANCODE_0:
+			keycode |= AKEY_5200_0;
+			break;
+		case SCANCODE_1:
+			keycode |= AKEY_5200_1;
+			break;
+		case SCANCODE_2:
+			keycode |= AKEY_5200_2;
+			break;
+		case SCANCODE_3:
+			keycode = key_shift ? AKEY_5200_HASH : AKEY_5200_3;
+			break;
+		case SCANCODE_4:
+			keycode |= AKEY_5200_4;
+			break;
+		case SCANCODE_5:
+			keycode |= AKEY_5200_5;
+			break;
+		case SCANCODE_6:
+			keycode |= AKEY_5200_6;
+			break;
+		case SCANCODE_7:
+			keycode |= AKEY_5200_7;
+			break;
+		case SCANCODE_8:
+			keycode = key_shift ? AKEY_5200_ASTERISK : AKEY_5200_8;
+			break;
+		case SCANCODE_9:
+			keycode |= AKEY_5200_9;
+			break;
+		case SCANCODE_EQUAL:
+			keycode = AKEY_5200_HASH;
+			break;
+		default:
+			keycode = AKEY_NONE;
+			break;
+		}
+		return keycode;
+	}
 
+	key_consol = (kbhits[SCANCODE_F2] ? 0 : CONSOL_OPTION)
+	           | (kbhits[SCANCODE_F3] ? 0 : CONSOL_SELECT)
+	           | (kbhits[SCANCODE_F4] ? 0 : CONSOL_START);
+
+	if (pause_hit) {
+		pause_hit = 0;
+		return AKEY_BREAK;
+	}
+
+	switch (kbcode) {
+	case SCANCODE_F6:
+		return AKEY_HELP;
+	case SCANCODE_F7:
+		return AKEY_BREAK;
 #define KBSCAN(name) \
-          case SCANCODE_##name: \
-            keycode |= (AKEY_##name & ~(AKEY_CTRL | AKEY_SHFT)); \
-            break;
-	  KBSCAN(ESCAPE)
-	  KBSCAN(1)
-	  case SCANCODE_2:
-            if (key_shift)
-              keycode = AKEY_AT;
-            else
-              keycode |= AKEY_2;
-            break;
-	  KBSCAN(3)
-	  KBSCAN(4)
-	  KBSCAN(5)
-	  case SCANCODE_6:
-            if (key_shift)
-              keycode = AKEY_CARET;
-            else
-              keycode |= AKEY_6;
-            break;
-	  case SCANCODE_7:
-            if (key_shift)
-              keycode = AKEY_AMPERSAND;
-            else
-              keycode |= AKEY_7;
-            break;
-	  case SCANCODE_8:
-            if (key_shift)
-              keycode = AKEY_ASTERISK;
-            else
-              keycode |= AKEY_8;
-            break;
-	  KBSCAN(9)
-	  KBSCAN(0)
-          KBSCAN(MINUS)
-          case SCANCODE_EQUAL:
-            if (key_shift)
-              keycode = AKEY_PLUS;
-            else
-              keycode |= AKEY_EQUAL;
-            break;
-          KBSCAN(BACKSPACE)
-          KBSCAN(TAB)
-	  KBSCAN(Q)
-	  KBSCAN(W)
-	  KBSCAN(E)
-	  KBSCAN(R)
-	  KBSCAN(T)
-	  KBSCAN(Y)
-	  KBSCAN(U)
-	  KBSCAN(I)
-	  KBSCAN(O)
-	  KBSCAN(P)
-	  case SCANCODE_BRACKET_LEFT:
-            keycode |= AKEY_BRACKETLEFT;
-            break;
-	  case SCANCODE_BRACKET_RIGHT:
-            keycode |= AKEY_BRACKETRIGHT;
-            break;
-          case SCANCODE_ENTER:
-            keycode |= AKEY_RETURN;
-            break;
-	  KBSCAN(A)
-	  KBSCAN(S)
-	  KBSCAN(D)
-	  KBSCAN(F)
-	  KBSCAN(G)
-	  KBSCAN(H)
-	  KBSCAN(J)
-	  KBSCAN(K)
-	  KBSCAN(L)
-	  KBSCAN(SEMICOLON)
-	  case SCANCODE_APOSTROPHE:
-            if (key_shift)
-              keycode = AKEY_DBLQUOTE;
-            else
-              keycode |= AKEY_QUOTE;
-            break;
-	  case SCANCODE_GRAVE:
-            keycode |= AKEY_ATARI;
-            break;
-	  case SCANCODE_BACKSLASH:
-            if (key_shift)
-              keycode = AKEY_EQUAL | AKEY_SHFT;
-            else
-              keycode |= AKEY_BACKSLASH;
-            break;
-	  KBSCAN(Z)
-	  KBSCAN(X)
-	  KBSCAN(C)
-	  KBSCAN(V)
-	  KBSCAN(B)
-	  KBSCAN(N)
-	  KBSCAN(M)
-	  case SCANCODE_COMMA:
-            if (key_shift)
-              keycode = AKEY_LESS;
-            else
-              keycode |= AKEY_COMMA;
-            break;
-	  case SCANCODE_PERIOD:
-            if (key_shift)
-              keycode = AKEY_GREATER;
-            else
-              keycode |= AKEY_FULLSTOP;
-            break;
-	  KBSCAN(SLASH)
-	  KBSCAN(SPACE)
-	  KBSCAN(CAPSLOCK)
-          case SCANCODE_CURSORBLOCKUP:
-            keycode = AKEY_UP;
-            break;
-          case SCANCODE_CURSORBLOCKDOWN:
-            keycode = AKEY_DOWN;
-            break;
-          case SCANCODE_CURSORBLOCKLEFT:
-            keycode = AKEY_LEFT;
-            break;
-          case SCANCODE_CURSORBLOCKRIGHT:
-            keycode = AKEY_RIGHT;
-            break;
-          case SCANCODE_REMOVE:
-            if (key_shift)
-              keycode = AKEY_DELETE_LINE;
-            else
-              keycode |= AKEY_DELETE_CHAR;
-            break;
-          case SCANCODE_INSERT:
-            if (key_shift)
-              keycode = AKEY_INSERT_LINE;
-            else
-              keycode |= AKEY_INSERT_CHAR;
-            break;
-          case SCANCODE_HOME:
-              keycode = AKEY_CLEAR;
-            break;
-          case SCANCODE_END:
-              keycode = AKEY_HELP;
-            break;
-          default:
-            keycode = AKEY_NONE;
-        }
+		case SCANCODE_##name: \
+			keycode |= (AKEY_##name & ~(AKEY_CTRL | AKEY_SHFT)); \
+			break;
+	KBSCAN(ESCAPE)
+	KBSCAN(1)
+	case SCANCODE_2:
+		if (key_shift)
+			keycode = AKEY_AT;
+		else
+			keycode |= AKEY_2;
+		break;
+	KBSCAN(3)
+	KBSCAN(4)
+	KBSCAN(5)
+	case SCANCODE_6:
+		if (key_shift)
+			keycode = AKEY_CARET;
+		else
+			keycode |= AKEY_6;
+		break;
+	case SCANCODE_7:
+		if (key_shift)
+			keycode = AKEY_AMPERSAND;
+		else
+			keycode |= AKEY_7;
+		break;
+	case SCANCODE_8:
+		if (key_shift)
+			keycode = AKEY_ASTERISK;
+		else
+			keycode |= AKEY_8;
+		break;
+	KBSCAN(9)
+	KBSCAN(0)
+	KBSCAN(MINUS)
+	case SCANCODE_EQUAL:
+		if (key_shift)
+			keycode = AKEY_PLUS;
+		else
+			keycode |= AKEY_EQUAL;
+		break;
+	KBSCAN(BACKSPACE)
+	KBSCAN(TAB)
+	KBSCAN(Q)
+	KBSCAN(W)
+	KBSCAN(E)
+	KBSCAN(R)
+	KBSCAN(T)
+	KBSCAN(Y)
+	KBSCAN(U)
+	KBSCAN(I)
+	KBSCAN(O)
+	KBSCAN(P)
+	case SCANCODE_BRACKET_LEFT:
+		keycode |= AKEY_BRACKETLEFT;
+		break;
+	case SCANCODE_BRACKET_RIGHT:
+		keycode |= AKEY_BRACKETRIGHT;
+		break;
+	case SCANCODE_ENTER:
+		keycode |= AKEY_RETURN;
+		break;
+	KBSCAN(A)
+	KBSCAN(S)
+	KBSCAN(D)
+	KBSCAN(F)
+	KBSCAN(G)
+	KBSCAN(H)
+	KBSCAN(J)
+	KBSCAN(K)
+	KBSCAN(L)
+	KBSCAN(SEMICOLON)
+	case SCANCODE_APOSTROPHE:
+		if (key_shift)
+			keycode = AKEY_DBLQUOTE;
+		else
+			keycode |= AKEY_QUOTE;
+		break;
+	case SCANCODE_GRAVE:
+		keycode |= AKEY_ATARI;
+		break;
+	case SCANCODE_BACKSLASH:
+		if (key_shift)
+			keycode = AKEY_BAR;
+		else
+			keycode |= AKEY_BACKSLASH;
+		break;
+	KBSCAN(Z)
+	KBSCAN(X)
+	KBSCAN(C)
+	KBSCAN(V)
+	KBSCAN(B)
+	KBSCAN(N)
+	KBSCAN(M)
+	case SCANCODE_COMMA:
+		if (key_shift)
+			keycode = AKEY_LESS;
+		else
+			keycode |= AKEY_COMMA;
+		break;
+	case SCANCODE_PERIOD:
+		if (key_shift)
+			keycode = AKEY_GREATER;
+		else
+			keycode |= AKEY_FULLSTOP;
+		break;
+	KBSCAN(SLASH)
+	KBSCAN(SPACE)
+	KBSCAN(CAPSLOCK)
+	case SCANCODE_CURSORBLOCKUP:
+		keycode = AKEY_UP;
+		break;
+	case SCANCODE_CURSORBLOCKDOWN:
+		keycode = AKEY_DOWN;
+		break;
+	case SCANCODE_CURSORBLOCKLEFT:
+		keycode = AKEY_LEFT;
+		break;
+	case SCANCODE_CURSORBLOCKRIGHT:
+		keycode = AKEY_RIGHT;
+		break;
+	case SCANCODE_REMOVE:
+		if (key_shift)
+			keycode = AKEY_DELETE_LINE;
+		else
+			keycode |= AKEY_DELETE_CHAR;
+		break;
+	case SCANCODE_INSERT:
+		if (key_shift)
+			keycode = AKEY_INSERT_LINE;
+		else
+			keycode |= AKEY_INSERT_CHAR;
+		break;
+	case SCANCODE_HOME:
+		keycode = AKEY_CLEAR;
+		break;
+	case SCANCODE_END:
+		keycode = AKEY_HELP;
+		break;
+	default:
+		keycode = AKEY_NONE;
+	}
 
 	return keycode;
 }
@@ -441,9 +486,6 @@ void Atari_Initialise(int *argc, char *argv[])
 {
 	int VGAMODE = G320x240x256;
 
-	/* int fd; */
-	/* int status; */
-
 	int i;
 	int j;
 
@@ -459,7 +501,7 @@ void Atari_Initialise(int *argc, char *argv[])
 		}
 		else {
 			if (strcmp(argv[i], "-help") == 0) {
-				Aprint("\t-interlace    Generate screen with interlace\n");
+				Aprint("\t-interlace       Generate screen with interlace");
 			}
 			argv[j++] = argv[i];
 		}
@@ -606,28 +648,28 @@ int Atari_Exit(int run_monitor)
 
 void Atari_DisplayScreen(UBYTE * screen)
 {
-  static int writepage = 0;
-  UBYTE *vbuf = screen + first_lno * ATARI_WIDTH + 32;
+	static int writepage = 0;
+	UBYTE *vbuf = screen + first_lno * ATARI_WIDTH + 32;
 
-  if (invisible)
-    goto after_screen_update;
+	if (invisible)
+		goto after_screen_update;
 #ifdef SVGA_SPEEDUP
-  vga_copytoplanar256(vbuf+ATARI_WIDTH*(240/refresh_rate)*writepage, ATARI_WIDTH,
-		      ((320 * (240/refresh_rate)) >> 2) * writepage
-		      , (320 >> 2), 320,
-		      240/refresh_rate);
-  vga_setdisplaystart(0);
-  writepage++;
-  if( writepage >= refresh_rate )
-	  writepage=0;
+	vga_copytoplanar256(vbuf + ATARI_WIDTH * (240 / refresh_rate) * writepage, ATARI_WIDTH,
+	                    ((320 * (240 / refresh_rate)) >> 2) * writepage,
+	                    320 >> 2, 320,
+	                    240 / refresh_rate);
+	vga_setdisplaystart(0);
+	writepage++;
+	if (writepage >= refresh_rate)
+		writepage = 0;
 #else
-  vga_copytoplanar256(vbuf, ATARI_WIDTH,
-		      ((320 * 240) >> 2) * writepage
-		      , 320 >> 2, 320,
-		      240);
-  vga_setdisplaystart(320 * 240 * writepage);
-  vga_setpage(0);
-  writepage ^= 1;
+	vga_copytoplanar256(vbuf, ATARI_WIDTH,
+	                    ((320 * 240) >> 2) * writepage,
+	                    320 >> 2, 320,
+	                    240);
+	vga_setdisplaystart(320 * 240 * writepage);
+	vga_setpage(0);
+	writepage ^= 1;
 #endif
 
 #ifdef SVGA_JOYMOUSE
@@ -761,6 +803,9 @@ int main(int argc, char **argv)
 
 /*
 $Log$
+Revision 1.18  2005/09/04 18:16:55  pfusik
+Atari 5200 keys
+
 Revision 1.17  2005/09/03 11:51:47  pfusik
 implemented F6 (Help) and F7 (Break)
 
