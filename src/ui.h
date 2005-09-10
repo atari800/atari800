1dd9158dae7a6b1ba281a0dab41246d6324aa9bc
Piotr Fusik <fox@scene.pl>
2005-09-10 12:27:09 +0000

    char * -> const char *

--- src/ui.h
+++ src/ui.h
@@ -26,12 +26,12 @@ extern UWORD crash_afterCIM;
    terminated by MENU_END */
 typedef struct
 {
-	char* sig;      /* Signature identifying item function. Conventionally, four-letter string */
-	long  flags;    /* Flags, see values below */
-	char* prefix;   /* Text to prepend the item */
-	char* item;     /* Main item text */
-	char* suffix;   /* Optional (not always) text to show after the item text. Often key shortcut */
-	int   retval;   /* Value returned by Select when this item is selected */
+	const char *sig;    /* Signature identifying item function. Conventionally, four-letter string */
+	long  flags;        /* Flags, see values below */
+	const char *prefix; /* Text to prepend the item */
+	const char *item;   /* Main item text */
+	const char *suffix; /* Optional (not always) text to show after the item text. Often key shortcut */
+	int   retval;       /* Value returned by Select when this item is selected */
 } tMenuItem;
 
 /* Set if item is enabled. Disabled items will not be shown */
@@ -60,20 +60,22 @@ typedef struct
 #define MENU_END { NULL, 0, NULL, NULL, NULL, 0 }
 
 /* UI driver entry prototypes */
-/* Select provides simple selection from menu. pTitle can be used as a caption. If bFloat!=0
-   the menu does not have caption and is of submenu type (or floating menu). nDefault is 
-   initially selected item (not item # but rather retval from menu structure. menu is array
-   of items of type tMenuItem, must be termintated by MENU_END. The last argument is used
-   to specify kind of selection for items with type modifier ITEM_MULTI. See below for possible
-   return values. */
-typedef int (*fnSelect)(char* pTitle, int bFloat, int nDefault, tMenuItem* menu, int* seltype);
+
+/* Select provides simple selection from menu.
+   pTitle can be used as a caption.
+   If bFloat!=0 the menu does not have caption and is of submenu type (or floating menu).
+   nDefault is initially selected item (not item # but rather retval from menu structure).
+   menu is array of items of type tMenuItem, must be termintated by MENU_END.
+   The last argument is used to specify kind of selection for items with type modifier ITEM_MULTI.
+   See below for possible return values. */
+typedef int (*fnSelect)(const char *pTitle, int bFloat, int nDefault, tMenuItem *menu, int *seltype);
 /* GetSaveFilename returns short filename in pFilename. It is guaranteed to be no more
    than FILENAME_SIZE characters in the name */
-typedef int (*fnGetSaveFilename)(char* pFilename);
+typedef int (*fnGetSaveFilename)(char *pFilename);
 /* GetLoadFilename returns fully qualified file name. Selection starts in pDirectory */
-typedef int (*fnGetLoadFilename)(char* pDirectory, char* pFilename);
+typedef int (*fnGetLoadFilename)(char *pDirectory, char *pFilename);
 /* Message is just some kind of MessageBox */
-typedef void (*fnMessage)(char* pMessage);
+typedef void (*fnMessage)(const char *pMessage);
 /* AboutBox shows information about emulator */
 typedef void (*fnAboutBox)(void);
 /* Init is called to initialize driver every time UI code is executed. Driver must be
@@ -97,7 +99,7 @@ typedef struct
 
 /* Current UI driver. Port can override it and set pointer to port
    specific driver */
-extern tUIDriver* ui_driver;
+extern tUIDriver *ui_driver;
 /* "Basic" UI driver. Always present and guaranteed to work. Override
    driver may call into this one to implement filtering */
 extern tUIDriver basic_ui_driver;
