df548866ebce9c2c3ac1a6a7e0788db17de64d60
Piotr Fusik <fox@scene.pl>
2005-10-09 20:13:47 +0000

    implemented Atari 5200 keys; F11 and Scroll Lock no longer press L on Atari keyboard; Ctrl+arrows emulate Atari arrows without Ctrl; adapted to Atari800 coding style

--- src/win32/atari_win32.c
+++ src/win32/atari_win32.c
@@ -35,6 +35,7 @@
 #include "monitor.h"
 #include "platform.h"
 #include "sound.h"
+#include "ui.h"
 
 #include "joystick.h"
 #include "keyboard.h"
@@ -43,206 +44,238 @@
 
 static int usesnd = 1;
 
-extern int alt_function;
 static int kbjoy = 0;
-static UBYTE joydefs[] =
+
+static const UBYTE joydefs[] =
 {
-  DIK_NUMPAD0,			/* fire */
-  DIK_NUMPAD7,			/* up/left */
-  DIK_NUMPAD8,			/* up */
-  DIK_NUMPAD9,			/* up/right */
-  DIK_NUMPAD4,			/* left */
-  DIK_NUMPAD6,			/* right */
-  DIK_NUMPAD1,			/* down/left */
-  DIK_NUMPAD2,			/* down */
-  DIK_NUMPAD3,			/* down/right */
+	DIK_NUMPAD0, /* fire */
+	DIK_NUMPAD7, /* up/left */
+	DIK_NUMPAD8, /* up */
+	DIK_NUMPAD9, /* up/right */
+	DIK_NUMPAD4, /* left */
+	DIK_NUMPAD6, /* right */
+	DIK_NUMPAD1, /* down/left */
+	DIK_NUMPAD2, /* down */
+	DIK_NUMPAD3, /* down/right */
 #ifdef USE_CURSORBLOCK
-  DIK_UP,
-  DIK_LEFT,
-  DIK_RIGHT,
-  DIK_DOWN,
+	DIK_UP,
+	DIK_LEFT,
+	DIK_RIGHT,
+	DIK_DOWN,
 #endif
 };
 
-static UBYTE joymask[] =
+static const UBYTE joymask[] =
 {
-  0,				/* not used */
-  ~1 & ~4,			/* up/left */
-  ~1,				/* up */
-  ~1 & ~8,			/* up/right */
-  ~4,				/* left */
-  ~8,				/* right */
-  ~2 & ~4,			/* down/left */
-  ~2,				/* down */
-  ~2 & ~8,			/* down/right */
+	STICK_CENTRE,  /* not used */
+	STICK_UL,      /* up/left */
+	STICK_FORWARD, /* up */
+	STICK_UR,      /* up/right */
+	STICK_LEFT,    /* left */
+	STICK_RIGHT,   /* right */
+	STICK_LL,      /* down/left */
+	STICK_BACK,    /* down */
+	STICK_LR,      /* down/right */
 #ifdef USE_CURSORBLOCK
-  ~1,				/* up */
-  ~4,				/* left */
-  ~8,				/* right */
-  ~2,				/* down */
+	STICK_FORWARD, /* up */
+	STICK_LEFT,    /* left */
+	STICK_RIGHT,   /* right */
+	STICK_BACK,    /* down */
 #endif
 };
 
 static int trig0 = 1;
 static int trig1 = 1;
-static int stick0;
-static int stick1;
+static int stick0 = STICK_CENTRE;
+static int stick1 = STICK_CENTRE;
 
 int Atari_Keyboard(void)
 {
-  int keycode;
-  int i;
+	int keycode;
+	int i;
 
-  prockb();
+	prockb();
 
-  stick0 |= 0xf;
-  stick1 |= 0xf;
-  if (kbjoy)
-    {
-      /* fire */
+	stick0 = STICK_CENTRE;
+	stick1 = STICK_CENTRE;
+	if (kbjoy) {
+		/* fire */
 #ifdef USE_CURSORBLOCK
-      trig0 = (kbhits[joydefs[0]] ? 0 : 1) & (kbhits[DIK_LCONTROL] ? 0 : 1);
+		trig0 = (kbhits[joydefs[0]] ? 0 : 1) & (kbhits[DIK_LCONTROL] ? 0 : 1);
 #else
-      trig0 = kbhits[joydefs[0]] ? 0 : 1;
+		trig0 = kbhits[joydefs[0]] ? 0 : 1;
 #endif
-      for (i = 1; i < sizeof(joydefs) / sizeof(joydefs[0]); i++)
-	if (kbhits[joydefs[i]])
-	  stick0 &= joymask[i];
-    }
-  else
-    {
-      if (!procjoy(0))
-        {
-          trig0 = !joystat.trig;
-          stick0 &= joystat.stick;
-        }
-      if (!procjoy(1))
-        {
-          trig1 = !joystat.trig;
-          stick1 &= joystat.stick;
-        }
-    }
-
-  key_consol = (key_consol & ~CONSOL_NONE)
-    | (kbhits[DIK_F2] ? 0 : CONSOL_OPTION)
-    | (kbhits[DIK_F3] ? 0 : CONSOL_SELECT)
-    | (kbhits[DIK_F4] ? 0 : CONSOL_START);
-
-  if (pause_hit)
-    {
-      pause_hit = 0;
-      return AKEY_BREAK;
-    }
-
-  key_shift = (kbhits[DIK_LSHIFT] | kbhits[DIK_RSHIFT]) ? 1 : 0;
-
-  alt_function = -1;		/* no alt function */
-  if (kbhits[DIK_LMENU])
-    {				/* left Alt key is pressed */
-      if (kbcode == DIK_R)
-	alt_function = MENU_RUN;	/* ALT+R .. Run file */
-      else if (kbcode == DIK_Y)
-	alt_function = MENU_SYSTEM;	/* ALT+Y .. Select system */
-      else if (kbcode == DIK_O)
-	alt_function = MENU_SOUND;	/* ALT+O .. mono/stereo sound */
-      else if (kbcode == DIK_A)
-	alt_function = MENU_ABOUT;	/* ALT+A .. About */
-      else if (kbcode == DIK_S)
-	alt_function = MENU_SAVESTATE;	/* ALT+S .. Save state */
-      else if (kbcode == DIK_D)
-	alt_function = MENU_DISK;	/* ALT+D .. Disk management */
-      else if (kbcode == DIK_L)
-	alt_function = MENU_LOADSTATE;	/* ALT+L .. Load state */
-      else if (kbcode == DIK_C)
-	alt_function = MENU_CARTRIDGE;	/* ALT+C .. Cartridge management */
-    }
-  if (alt_function != -1)
-    return AKEY_UI;
-
-  /* need to set shift mask here to avoid conflict with PC layout */
-  keycode = (key_shift ? 0x40 : 0)
-    | ((kbhits[DIK_LCONTROL]
-	| kbhits[DIK_RCONTROL]) ? 0x80 : 0);
-
-  switch (kbcode)
-    {
-    case DIK_F9:
-      return AKEY_EXIT;
-    case DIK_F8:
-      keycode = Atari_Exit(1) ? AKEY_NONE : AKEY_EXIT;
-      kbcode = 0;
-      break;
-    case DIK_SCROLL:		/* pause */
-      while (kbhits[DIK_SCROLL])
-	prockb();
-      while (!kbhits[DIK_SCROLL])
-	prockb();
-      kbcode = 0;
-      break;
-    case DIK_SYSRQ:
-    case DIK_F10:
-      keycode = key_shift ? AKEY_SCREENSHOT_INTERLACE : AKEY_SCREENSHOT;
-      kbcode = 0;
-      break;
-
-    case DIK_F1:
-      return AKEY_UI;
-    case DIK_F5:
-      return key_shift ? AKEY_COLDSTART : AKEY_WARMSTART;
-    case DIK_F7:
-      return AKEY_BREAK;
-    case DIK_F11:
-      for (i = 0; i < 4; i++)
-	{
-	  if (++joy_autofire[i] > 2)
-	    joy_autofire[i] = 0;
+		for (i = 1; i < sizeof(joydefs) / sizeof(joydefs[0]); i++)
+			if (kbhits[joydefs[i]])
+				stick0 &= joymask[i];
 	}
-      kbcode = 0;
-      break;
+	else {
+		if (!procjoy(0)) {
+			trig0 = !joystat.trig;
+			stick0 &= joystat.stick;
+		}
+		if (!procjoy(1)) {
+			trig1 = !joystat.trig;
+			stick1 &= joystat.stick;
+		}
+	}
 
+	key_consol = (kbhits[DIK_F2] ? 0 : CONSOL_OPTION)
+	           + (kbhits[DIK_F3] ? 0 : CONSOL_SELECT)
+	           + (kbhits[DIK_F4] ? 0 : CONSOL_START);
+
+	if (pause_hit) {
+		pause_hit = 0;
+		return AKEY_BREAK;
+	}
+
+	key_shift = (kbhits[DIK_LSHIFT] | kbhits[DIK_RSHIFT]) ? 1 : 0;
+
+	if (kbhits[DIK_LMENU]) { /* left Alt key is pressed */
+		switch (kbcode) {
+		case DIK_R:
+			alt_function = MENU_RUN;       /* ALT+R .. Run file */
+			return AKEY_UI;
+		case DIK_Y:
+			alt_function = MENU_SYSTEM;    /* ALT+Y .. Select system */
+			return AKEY_UI;
+		case DIK_O:
+			alt_function = MENU_SOUND;     /* ALT+O .. mono/stereo sound */
+			return AKEY_UI;
+		case DIK_A:
+			alt_function = MENU_ABOUT;     /* ALT+A .. About */
+			return AKEY_UI;
+		case DIK_S:
+			alt_function = MENU_SAVESTATE; /* ALT+S .. Save state */
+			return AKEY_UI;
+		case DIK_D:
+			alt_function = MENU_DISK;      /* ALT+D .. Disk management */
+			return AKEY_UI;
+		case DIK_L:
+			alt_function = MENU_LOADSTATE; /* ALT+L .. Load state */
+			return AKEY_UI;
+		case DIK_C:
+			alt_function = MENU_CARTRIDGE; /* ALT+C .. Cartridge management */
+			return AKEY_UI;
+		default:
+			break;
+		}
+	}
+
+	switch (kbcode) {
+	case DIK_F1:
+		return AKEY_UI;
+	case DIK_F5:
+		return key_shift ? AKEY_COLDSTART : AKEY_WARMSTART;
+	case DIK_F7:
+		return AKEY_BREAK;
+	case DIK_F8:
+		keycode = Atari_Exit(1) ? AKEY_NONE : AKEY_EXIT;
+		kbcode = 0;
+		return keycode;
+	case DIK_F9:
+		return AKEY_EXIT;
+	case DIK_SYSRQ:
+	case DIK_F10:
+		kbcode = 0;
+		return key_shift ? AKEY_SCREENSHOT_INTERLACE : AKEY_SCREENSHOT;
+	case DIK_F11:
+		for (i = 0; i < 4; i++) {
+			if (++joy_autofire[i] > 2)
+				joy_autofire[i] = 0;
+		}
+		kbcode = 0;
+		return AKEY_NONE;
+	case DIK_SCROLL: /* Scroll Lock = pause */
+		while (kbhits[DIK_SCROLL])
+			prockb();
+		while (!kbhits[DIK_SCROLL])
+			prockb();
+		kbcode = 0;
+		return AKEY_NONE;
+	default:
+		break;
+	}
+
+	if (machine_type == MACHINE_5200 && !ui_is_active) {
+		keycode = (key_shift ? 0x40 : 0);
+		switch (kbcode) {
+#define KBSCAN_5200(name) \
+			case DIK_##name: \
+				return AKEY_5200_##name + keycode;
+		case DIK_F4:
+			return AKEY_5200_START + keycode;
+		case DIK_P:
+			return AKEY_5200_PAUSE + keycode;
+		case DIK_R:
+			return AKEY_5200_RESET + keycode;
+		KBSCAN_5200(1)
+		KBSCAN_5200(2)
+		case DIK_3:
+			return key_shift ? AKEY_5200_HASH : AKEY_5200_3;
+		KBSCAN_5200(4)
+		KBSCAN_5200(5)
+		KBSCAN_5200(6)
+		KBSCAN_5200(7)
+		case DIK_8:
+			return key_shift ? AKEY_5200_ASTERISK : AKEY_5200_8;
+		KBSCAN_5200(9)
+		KBSCAN_5200(0)
+		case DIK_EQUALS:
+			return AKEY_5200_HASH + keycode;
+		default:
+			return AKEY_NONE;
+		}
+	}
+
+	/* need to set shift mask here to avoid conflict with PC layout */
+	keycode = (key_shift ? 0x40 : 0)
+	        + ((kbhits[DIK_LCONTROL] | kbhits[DIK_RCONTROL]) ? 0x80 : 0);
+
+	switch (kbcode) {
 #define KBSCAN(name) \
-	  case DIK_##name: \
-	    keycode |= (AKEY_##name & ~(AKEY_CTRL | AKEY_SHFT)); \
-	    break;
-      KBSCAN(ESCAPE)
+		case DIK_##name: \
+			keycode |= (AKEY_##name & ~AKEY_SHFTCTRL); \
+			break;
+	KBSCAN(ESCAPE)
 	KBSCAN(1)
-    case DIK_2:
-      if (key_shift)
-	keycode = AKEY_AT;
-      else
-	keycode |= AKEY_2;
-      break;
-      KBSCAN(3)
+	case DIK_2:
+		if (key_shift)
+			keycode = AKEY_AT;
+		else
+			keycode |= AKEY_2;
+		break;
+	KBSCAN(3)
 	KBSCAN(4)
 	KBSCAN(5)
-    case DIK_6:
-      if (key_shift)
-	keycode = AKEY_CARET;
-      else
-	keycode |= AKEY_6;
-      break;
-    case DIK_7:
-      if (key_shift)
-	keycode = AKEY_AMPERSAND;
-      else
-	keycode |= AKEY_7;
-      break;
-    case DIK_8:
-      if (key_shift)
-	keycode = AKEY_ASTERISK;
-      else
-	keycode |= AKEY_8;
-      break;
-      KBSCAN(9)
+	case DIK_6:
+		if (key_shift)
+			keycode = AKEY_CARET;
+		else
+			keycode |= AKEY_6;
+		break;
+	case DIK_7:
+		if (key_shift)
+			keycode = AKEY_AMPERSAND;
+		else
+			keycode |= AKEY_7;
+		break;
+	case DIK_8:
+		if (key_shift)
+			keycode = AKEY_ASTERISK;
+		else
+			keycode |= AKEY_8;
+		break;
+	KBSCAN(9)
 	KBSCAN(0)
 	KBSCAN(MINUS)
-    case DIK_EQUALS:
-      if (key_shift)
-	keycode = AKEY_PLUS;
-      else
-	keycode |= AKEY_EQUAL;
-      break;
-      KBSCAN(BACKSPACE)
+	case DIK_EQUALS:
+		if (key_shift)
+			keycode = AKEY_PLUS;
+		else
+			keycode |= AKEY_EQUAL;
+		break;
+	KBSCAN(BACKSPACE)
 	KBSCAN(TAB)
 	KBSCAN(Q)
 	KBSCAN(W)
@@ -254,16 +287,14 @@ int Atari_Keyboard(void)
 	KBSCAN(I)
 	KBSCAN(O)
 	KBSCAN(P)
-    case DIK_LBRACKET:
-      keycode |= AKEY_BRACKETLEFT;
-      break;
-    case DIK_RBRACKET:
-      keycode |= AKEY_BRACKETRIGHT;
-      break;
-    case DIK_RETURN:
-      keycode |= AKEY_RETURN;
-      break;
-      KBSCAN(A)
+	case DIK_LBRACKET:
+		keycode |= AKEY_BRACKETLEFT;
+		break;
+	case DIK_RBRACKET:
+		keycode |= AKEY_BRACKETRIGHT;
+		break;
+	KBSCAN(RETURN)
+	KBSCAN(A)
 	KBSCAN(S)
 	KBSCAN(D)
 	KBSCAN(F)
@@ -273,201 +304,189 @@ int Atari_Keyboard(void)
 	KBSCAN(K)
 	KBSCAN(L)
 	KBSCAN(SEMICOLON)
-    case DIK_APOSTROPHE:
-      if (key_shift)
-	keycode = AKEY_DBLQUOTE;
-      else
-	keycode |= AKEY_QUOTE;
-      break;
-    case DIK_GRAVE:
-      keycode |= AKEY_ATARI;
-      break;
-    case DIK_BACKSLASH:
-      if (key_shift)
-	keycode = AKEY_EQUAL | AKEY_SHFT;
-      else
-	keycode |= AKEY_BACKSLASH;
-      break;
-      KBSCAN(Z)
+	case DIK_APOSTROPHE:
+		if (key_shift)
+			keycode = AKEY_DBLQUOTE;
+		else
+			keycode |= AKEY_QUOTE;
+		break;
+	case DIK_GRAVE:
+		keycode |= AKEY_ATARI;
+		break;
+	case DIK_BACKSLASH:
+		if (key_shift)
+			keycode = AKEY_BAR;
+		else
+			keycode |= AKEY_BACKSLASH;
+		break;
+	KBSCAN(Z)
 	KBSCAN(X)
 	KBSCAN(C)
 	KBSCAN(V)
 	KBSCAN(B)
 	KBSCAN(N)
 	KBSCAN(M)
-    case DIK_COMMA:
-      if (key_shift)
-	keycode = AKEY_LESS;
-      else
-	keycode |= AKEY_COMMA;
-      break;
-    case DIK_PERIOD:
-      if (key_shift)
-	keycode = AKEY_GREATER;
-      else
-	keycode |= AKEY_FULLSTOP;
-      break;
-      KBSCAN(SLASH)
+	case DIK_COMMA:
+		if (key_shift)
+			keycode = AKEY_LESS;
+		else
+			keycode |= AKEY_COMMA;
+		break;
+	case DIK_PERIOD:
+		if (key_shift)
+			keycode = AKEY_GREATER;
+		else
+			keycode |= AKEY_FULLSTOP;
+		break;
+	KBSCAN(SLASH)
 	KBSCAN(SPACE)
 	KBSCAN(CAPSLOCK)
-    case DIK_UP:
-      keycode = AKEY_UP;
-      break;
-    case DIK_DOWN:
-      keycode = AKEY_DOWN;
-      break;
-    case DIK_LEFT:
-      keycode = AKEY_LEFT;
-      break;
-    case DIK_RIGHT:
-      keycode = AKEY_RIGHT;
-      break;
-    case DIK_DELETE:
-      if (key_shift)
-	keycode = AKEY_DELETE_LINE;
-      else
-	keycode |= AKEY_DELETE_CHAR;
-      break;
-    case DIK_INSERT:
-      if (key_shift)
-	keycode = AKEY_INSERT_LINE;
-      else
-	keycode |= AKEY_INSERT_CHAR;
-      break;
-    case DIK_HOME:
-      keycode = AKEY_CLEAR;
-      break;
-    case DIK_F6:
-    case DIK_END:
-      keycode = AKEY_HELP;
-      break;
-    default:
-      keycode = AKEY_NONE;
-    }
-
-  return keycode;
+	case DIK_UP:
+		keycode ^= AKEY_UP;
+		break;
+	case DIK_DOWN:
+		keycode ^= AKEY_DOWN;
+		break;
+	case DIK_LEFT:
+		keycode ^= AKEY_LEFT;
+		break;
+	case DIK_RIGHT:
+		keycode ^= AKEY_RIGHT;
+		break;
+	case DIK_DELETE:
+		if (key_shift)
+			keycode = AKEY_DELETE_LINE;
+		else
+			keycode |= AKEY_DELETE_CHAR;
+		break;
+	case DIK_INSERT:
+		if (key_shift)
+			keycode = AKEY_INSERT_LINE;
+		else
+			keycode |= AKEY_INSERT_CHAR;
+		break;
+	case DIK_HOME:
+		keycode = AKEY_CLEAR;
+		break;
+	case DIK_F6:
+	case DIK_END:
+		keycode = AKEY_HELP;
+		break;
+	default:
+		keycode = AKEY_NONE;
+		break;
+	}
+	return keycode;
 }
 
 void Atari_Initialise(int *argc, char *argv[])
 {
-  int i;
-  int j;
-  int help_only = FALSE;
+	int i;
+	int j;
+	int help_only = FALSE;
 
-  for (i = j = 1; i < *argc; i++)
-  {
-    if (strcmp(argv[i], "-nojoystick") == 0)
-    {
-      kbjoy = 1;
-      Aprint("no joystick");
-    }
-    else
-    {
-      if (strcmp(argv[i], "-help") == 0)
-      {
-        help_only = TRUE;
-        Aprint("\t-nojoystick      Disable joystick");
-      }
-      argv[j++] = argv[i];
-    }
-  }
+	for (i = j = 1; i < *argc; i++) {
+		if (strcmp(argv[i], "-nojoystick") == 0) {
+			kbjoy = 1;
+			Aprint("no joystick");
+		}
+		else {
+			if (strcmp(argv[i], "-help") == 0) {
+				help_only = TRUE;
+				Aprint("\t-nojoystick      Disable joystick");
+			}
+		argv[j++] = argv[i];
+		}
+	}
 
-  *argc = j;
+	*argc = j;
 
 #ifdef SOUND
-  if (usesnd)
-    Sound_Initialise(argc, argv);
+	if (usesnd)
+		Sound_Initialise(argc, argv);
 #endif
 
-  if (gron(argc, argv))
-    exit(1);
+	if (gron(argc, argv))
+		exit(1);
 
-  if (help_only)
-    return;
+	if (help_only)
+		return;
 
-  if (initinput())
-  {
-    MessageBox(hWndMain, "DirectInput Init FAILED",
-		myname,
-		MB_ICONEXCLAMATION
-		| MB_OK);
-    exit(1);
-  }
-  clearkb();
+	if (initinput()) {
+		MessageBox(hWndMain, "DirectInput Init FAILED",
+		           myname, MB_ICONEXCLAMATION | MB_OK);
+		exit(1);
+	}
+	clearkb();
 
-  if (!kbjoy)
-  {
-    if (initjoystick())
-    {
-      kbjoy = 1;
-      Aprint("joystick not found");
-    }
-  }
+	if (!kbjoy) {
+		if (initjoystick()) {
+			kbjoy = 1;
+			Aprint("joystick not found");
+		}
+	}
 
-  trig0 = 1;
-  stick0 = 15;
-  key_consol = CONSOL_NONE;
+	trig0 = 1;
+	stick0 = 15;
+	key_consol = CONSOL_NONE;
 
-  ShowWindow(hWndMain, SW_RESTORE);
+	ShowWindow(hWndMain, SW_RESTORE);
 }
 
 int Atari_Exit(int run_monitor)
 {
-  int i;
+	Aflushlog();
 
-  Aflushlog();
-
-  if (run_monitor)
-    {
+	if (run_monitor) {
+		int i;
 #ifdef SOUND
-      Sound_Pause();
+		Sound_Pause();
 #endif
-      ShowWindow(hWndMain, SW_MINIMIZE);
-      i = monitor();
-      ShowWindow(hWndMain, SW_RESTORE);
+		ShowWindow(hWndMain, SW_MINIMIZE);
+		i = monitor();
+		ShowWindow(hWndMain, SW_RESTORE);
 #ifdef SOUND
-      Sound_Continue();
+		Sound_Continue();
 #endif
-      if (i)
-	return 1;	      /* return to emulation */
-    }
+		if (i)
+			return TRUE; /* return to emulation */
+	}
 
-  return 0;
+	return FALSE;
 }
 
-void Atari_DisplayScreen(UBYTE * ascreen)
+void Atari_DisplayScreen(UBYTE *ascreen)
 {
-  refreshv(ascreen + 24);
+	refreshv(ascreen + 24);
 }
 
 int Atari_PORT(int num)
 {
-  if (num == 0)
-  {
-    return (stick1 << 4) | stick0;
-  }
-  else
-  {
-    return 0xff;
-  }
+	if (num == 0)
+		return (stick1 << 4) | stick0;
+	return 0xff;
 }
-
 
 int Atari_TRIG(int num)
 {
-  switch (num)
-  {
-  case 0:
-    return trig0;
-  case 1:
-    return trig1;
-  default:
-    return 1;
-  }
+	switch (num) {
+	case 0:
+		return trig0;
+	case 1:
+		return trig1;
+	default:
+		return 1;
+	}
 }
 
 /*
 $Log$
+Revision 1.14  2005/10/09 20:13:47  pfusik
+implemented Atari 5200 keys;
+F11 and Scroll Lock no longer press L on Atari keyboard;
+Ctrl+arrows emulate Atari arrows without Ctrl;
+adapted to Atari800 coding style
+
 Revision 1.13  2005/09/11 20:43:50  pfusik
 F6 = Help, F7 = Break
 
