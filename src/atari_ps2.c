7a480d040cd50076138886d95b23e682ace40b6f
Troy Ayers <holiday42@gmail.com>
2005-10-14 03:11:17 +0000

    Changed atari_ps2.c to use rawkeyboard mode.  Updated configure.ac PS2 section to remove check for libgcc, and added check for libz

--- src/atari_ps2.c
+++ src/atari_ps2.c
@@ -53,6 +53,11 @@ static GSGLOBAL *gsGlobal = NULL;
 
 static int clut[256] __attribute__((aligned(16)));
 
+//int PS2KbdCAPS=0;
+int PS2KbdSHIFT=0;
+int PS2KbdCONTROL=0;
+
+
 #define PAD_PORT 0
 #define PAD_SLOT 0
 
@@ -98,8 +103,8 @@ void loadModules(void)
 	if (PS2KbdInit() == 0) {
 		Aprint("Failed to Init Keyboard.");
 	}
-//	PS2KbdSetReadmode(PS2KBD_READMODE_RAW);
-	PS2KbdSetReadmode(PS2KBD_READMODE_NORMAL);
+	PS2KbdSetReadmode(PS2KBD_READMODE_RAW);
+//	PS2KbdSetReadmode(PS2KBD_READMODE_NORMAL);
 //	Aprint("keyboard read mode is %d", PS2KBD_READMODE_NORMAL);
 }
 
@@ -109,7 +114,8 @@ void Atari_Initialise(int *argc, char *argv[])
 	int i;
 	for (i = 0; i < 256; i++) {
 		int c = colortable[i];
-		clut[i] = (c >> 16) + (c & 0xff00) + ((c & 0xff) << 16);
+//		clut[i] = (c >> 16) + (c & 0xff00) + ((c & 0xff) << 16);
+	clut[(i ^ i * 2) & 16 ? i ^ 24 : i] = (c >> 16) + (c & 0xff00) + ((c & 0xff) << 16);
 	}
 	// Clear debug from screen
 	init_scr();
@@ -136,6 +142,12 @@ int Atari_Exit(int run_monitor)
 		return TRUE;
 	}
 #endif
+//zzz temp exit procedure 
+//Hard coded to go back to ulaunch
+    fioExit();
+    SifExitRpc();
+    LoadExecPS2("mc0:/BOOT/BOOT.ELF", 0, NULL);
+//zzz end
 	return FALSE;
 }
 
@@ -174,6 +186,7 @@ static int PadButtons(void)
 int Atari_Keyboard(void)
 {
 	int new_pad = PadButtons();
+	PS2KbdRawKey key;
 	key_consol = CONSOL_NONE;
 
 	if (ui_is_active) {
@@ -189,13 +202,22 @@ int Atari_Keyboard(void)
 			return AKEY_UP;
 		if (new_pad & PAD_DOWN)
 			return AKEY_DOWN;
-	}
-	else {
-		if (new_pad & PAD_TRIANGLE)
-			return AKEY_UI;
-	}
-	if (new_pad & PAD_L1)
-		return AKEY_COLDSTART;
+		if (new_pad & PAD_L1)
+		    return AKEY_COLDSTART;
+		if (new_pad & PAD_R1)
+			return AKEY_WARMSTART;
+        }
+        //PAD_CROSS is used for Atari_TRIG().
+	if (new_pad & PAD_TRIANGLE)
+		return AKEY_UI;
+        if (new_pad & PAD_SQUARE)
+			return AKEY_SPACE;
+	if (new_pad & PAD_CIRCLE)
+		return AKEY_RETURN;
+        if (new_pad & PAD_L1)
+			return AKEY_COLDSTART;
+	if (new_pad & PAD_R1)
+		return AKEY_WARMSTART;
 	if (machine_type == MACHINE_5200) {
 		if (new_pad & PAD_START)
 			return AKEY_5200_START;
@@ -209,211 +231,368 @@ int Atari_Keyboard(void)
 			return AKEY_HELP;
 	}
 	if (machine_type != MACHINE_5200 || ui_is_active) {
-		char key;
-		while (PS2KbdRead(&key) != 0) {
-			if (key == PS2KBD_ESCAPE_KEY)
-				PS2KbdRead(&key);
-			switch (key) {
+
+		while (PS2KbdReadRaw(&key) != 0) {
+		    if(key.state == PS2KBD_RAWKEY_DOWN){
+			switch (key.key) {
 			case EOF:
-				Atari800_Exit(FALSE);
-				exit(0);
-				break;
-			case 'A':
-				return AKEY_A;
-			case 'B':
-				return AKEY_B;
-			case 'C':
-				return AKEY_C;
-			case 'D':
-				return AKEY_D;
-			case 'E':
-				return AKEY_E;
-			case 'F':
-				return AKEY_F;
-			case 'G':
-				return AKEY_G;
-			case 'H':
-				return AKEY_H;
-			case 'I':
-				return AKEY_I;
-			case 'J':
-				return AKEY_J;
-			case 'K':
-				return AKEY_K;
-			case 'L':
-				return AKEY_L;
-			case 'M':
-				return AKEY_M;
-			case 'N':
-				return AKEY_N;
-			case 'O':
-				return AKEY_O;
-			case 'P':
-				return AKEY_P;
-			case 'Q':
-				return AKEY_Q;
-			case 'R':
-				return AKEY_R;
-			case 'S':
-				return AKEY_S;
-			case 'T':
-				return AKEY_T;
-			case 'U':
-				return AKEY_U;
-			case 'V':
-				return AKEY_V;
-			case 'W':
-				return AKEY_W;
-			case 'X':
-				return AKEY_X;
-			case 'Y':
-				return AKEY_Y;
-			case 'Z':
-				return AKEY_Z;
-			case 'a':
-				return AKEY_a;
-			case 'b':
-				return AKEY_b;
-			case 'c':
-				return AKEY_c;
-			case 'd':
-				return AKEY_d;
-			case 'e':
-				return AKEY_e;
-			case 'f':
-				return AKEY_f;
-			case 'g':
-				return AKEY_g;
-			case 'h':
-				return AKEY_h;
-			case 'i':
-				return AKEY_i;
-			case 'j':
-				return AKEY_j;
-			case 'k':
-				return AKEY_k;
-			case 'l':
-				return AKEY_l;
-			case 'm':
-				return AKEY_m;
-			case 'n':
-				return AKEY_n;
-			case 'o':
-				return AKEY_o;
-			case 'p':
-				return AKEY_p;
-			case 'q':
-				return AKEY_q;
-			case 'r':
-				return AKEY_r;
-			case 's':
-				return AKEY_s;
-			case 't':
-				return AKEY_t;
-			case 'u':
-				return AKEY_u;
-			case 'v':
-				return AKEY_v;
-			case 'w':
-				return AKEY_w;
-			case 'x':
-				return AKEY_x;
-			case 'y':
-				return AKEY_y;
-			case 'z':
-				return AKEY_z;
-			case '0':
-				return AKEY_0;
-			case '1':
-				return AKEY_1;
-			case '2':
-				return AKEY_2;
-			case '3':
-				return AKEY_3;
-			case '4':
-				return AKEY_4;
-			case '5':
-				return AKEY_5;
-			case '6':
-				return AKEY_6;
-			case '7':
-				return AKEY_7;
-			case '8':
-				return AKEY_8;
-			case '9':
-				return AKEY_9;
-			case ' ':
-				return AKEY_SPACE;
-			case '`':
-				return AKEY_CAPSTOGGLE;
-			case '!':
-				return AKEY_EXCLAMATION;
-			case '"':
-				return AKEY_DBLQUOTE;
-			case '#':
-				return AKEY_HASH;
-			case '$':
-				return AKEY_DOLLAR;
-			case '%':
-				return AKEY_PERCENT;
-			case '&':
-				return AKEY_AMPERSAND;
-			case '\'':
-				return AKEY_QUOTE;
-			case '@':
-				return AKEY_AT;
-			case '(':
-				return AKEY_PARENLEFT;
-			case ')':
-				return AKEY_PARENRIGHT;
-			case '[':
-				return AKEY_BRACKETLEFT;
-			case ']':
-				return AKEY_BRACKETRIGHT;
-			case '<':
-				return AKEY_LESS;
-			case '>':
-				return AKEY_GREATER;
-			case '=':
-				return AKEY_EQUAL;
-			case '?':
-				return AKEY_QUESTION;
-			case '-':
-				return AKEY_MINUS;
-			case '+':
-				return AKEY_PLUS;
-			case '*':
-				return AKEY_ASTERISK;
-			case '/':
-				return AKEY_SLASH;
-			case ':':
-				return AKEY_COLON;
-			case ';':
-				return AKEY_SEMICOLON;
-			case ',':
-				return AKEY_COMMA;
-			case '.':
-				return AKEY_FULLSTOP;
-			case '_':
-				return AKEY_UNDERSCORE;
-			case '^':
-				return AKEY_CIRCUMFLEX;
-			case '\\':
-				return AKEY_BACKSLASH;
-			case '|':
-				return AKEY_BAR;
-			case '\n':
+			    Atari800_Exit(FALSE);
+			    exit(0);
+			    break;
+			case 0x28:
 				return AKEY_RETURN;
-			case '\t':
-				return AKEY_TAB;
-			case '\b':
+			case 0x29:
+			    return AKEY_ESCAPE;
+			case 0x2A:
 				return AKEY_BACKSPACE;
-			// TODO: control keys, arrows
+			case 0x2B:
+				return AKEY_TAB;
+			case 0x2C:
+				return AKEY_SPACE;
+			case 0x46:
+				return AKEY_SCREENSHOT;
+			case 0x4F:
+			    return AKEY_RIGHT;
+			case 0x50:
+			    return AKEY_LEFT;
+			case 0x51:
+			    return AKEY_DOWN;
+			case 0x52:
+			    return AKEY_UP;
+			case 0x58:
+			    return AKEY_RETURN;
+
+			case 0xE0:
+				PS2KbdCONTROL=1;
+				return AKEY_NONE;
+			case 0xE4:
+				PS2KbdCONTROL=1;
+				return AKEY_NONE;
+			case 0xE1:
+				PS2KbdSHIFT=1;
+				return AKEY_NONE;
+			case 0xE5:
+				PS2KbdSHIFT=1;
+				return AKEY_NONE;
+			//todo: map the following keys
+			//control <>: (suits)
+			//akey_clear
+			//AKEY_BREAK
+			//control-1 = pause??(is this break?)
+			//control-2 = bell (clear?)
+			//control-3 = error - 136? (break?)
+			//akey_caret && circumflex difference???
+			//TODO:enable keyboard repeat when shift nor control pressed
+                        default:
+				break;
+			}
+		}
+                    
+		if((key.state == PS2KBD_RAWKEY_DOWN) && (!PS2KbdSHIFT))
+		{
+		switch (key.key) {
+			case 0x1E:
+				return AKEY_1;
+			case 0X1F:
+				return AKEY_2;
+			case 0x20:
+				return AKEY_3;
+			case 0x21:
+				return AKEY_4;
+			case 0x22:
+				return AKEY_5;
+			case 0x23:
+				return AKEY_6;
+			case 0x24:
+				return AKEY_7;
+			case 0x25:
+				return AKEY_8;
+			case 0x26:
+				return AKEY_9;
+			case 0x27:
+				return AKEY_0;
+			case 0x2D:
+				return AKEY_MINUS;
+			case 0x2E:
+				return AKEY_EQUAL;
+			case 0x2F:
+				return AKEY_BRACKETLEFT;
+			case 0x30:
+				return AKEY_BRACKETRIGHT;
+			case 0x31:
+				return AKEY_BACKSLASH;
+			case 0x33:
+				return AKEY_SEMICOLON;
+			case 0x34:
+				return AKEY_QUOTE;
+			case 0x35:
+				return AKEY_ATARI;
+			case 0x36:
+				return AKEY_COMMA;
+			case 0x37:
+				return AKEY_FULLSTOP;
+			case 0x38:
+				return AKEY_SLASH;
+			case 0x3A:
+				return AKEY_UI;
+			case 0x3E:
+				return AKEY_WARMSTART;
+			case 0x41:
+				return AKEY_EXIT;
+			case 0x43:
+				return AKEY_SCREENSHOT;
+			default:
+				break;
+			}
+		}
+		if((key.state == PS2KBD_RAWKEY_DOWN) && PS2KbdSHIFT && !PS2KbdCONTROL){
+		switch (key.key) {
+			case 0x4:
+				return AKEY_A;
+			case 0x5:
+				return AKEY_B;
+			case 0x6:
+				return AKEY_C;
+			case 0x7:
+				return AKEY_D;
+			case 0x8:
+				return AKEY_E;
+			case 0x9:
+				return AKEY_F;
+			case 0xA:
+				return AKEY_G;
+			case 0xB:
+				return AKEY_H;
+			case 0xC:
+				return AKEY_I;
+			case 0xD:
+				return AKEY_J;
+			case 0xE:
+				return AKEY_K;
+			case 0xF:
+				return AKEY_L;
+			case 0x10:
+				return AKEY_M;
+			case 0x11:
+				return AKEY_N;
+			case 0x12:
+				return AKEY_O;
+			case 0x13:
+				return AKEY_P;
+			case 0x14:
+				return AKEY_Q;
+			case 0x15:
+				return AKEY_R;
+			case 0x16:
+				return AKEY_S;
+			case 0x17:
+				return AKEY_T;
+			case 0x18:
+				return AKEY_U;
+			case 0x19:
+				return AKEY_V;
+			case 0x1A:
+				return AKEY_W;
+			case 0x1B:
+				return AKEY_X;
+			case 0x1C:
+				return AKEY_Y;
+			case 0x1D:
+				return AKEY_Z;
+			case 0x1E:
+				return AKEY_EXCLAMATION;
+			case 0X1F:
+				return AKEY_AT;
+			case 0x20:
+				return AKEY_HASH;
+			case 0x21:
+				return AKEY_DOLLAR;
+			case 0x22:
+				return AKEY_PERCENT;
+			case 0x23:
+//				return AKEY_CIRCUMFLEX;
+				return AKEY_CARET;
+			case 0x24:
+				return AKEY_AMPERSAND;
+			case 0x25:
+				return AKEY_ASTERISK;
+			case 0x26:
+				return AKEY_PARENLEFT;
+			case 0x27:
+				return AKEY_PARENRIGHT;
+			case 0x2B:
+				return AKEY_SETTAB;
+			case 0x2D:
+				return AKEY_UNDERSCORE;
+			case 0x2E:
+				return AKEY_PLUS;
+			case 0x31:
+				return AKEY_BAR;
+			case 0x33:
+				return AKEY_COLON;
+			case 0x34:
+				return AKEY_DBLQUOTE;
+			case 0x36:
+				return AKEY_LESS;
+			case 0x37:
+				return AKEY_GREATER;
+			case 0x38:
+				return AKEY_QUESTION;
+			case 0x3E:
+				return AKEY_COLDSTART;
+			case 0x49:
+				return AKEY_INSERT_LINE;
+			case 0x4C:
+				return AKEY_DELETE_LINE;
+			default:
+				break;
+			}
+		}
+		if((key.state == PS2KBD_RAWKEY_DOWN) && !PS2KbdSHIFT && !PS2KbdCONTROL){
+			switch(key.key){
+			case 0x4:
+				return AKEY_a;
+			case 0x5:
+				return AKEY_b;
+			case 0x6:
+				return AKEY_c;
+			case 0x7:
+				return AKEY_d;
+			case 0x8:
+				return AKEY_e;
+			case 0x9:
+				return AKEY_f;
+			case 0xA:
+				return AKEY_g;
+			case 0xB:
+				return AKEY_h;
+			case 0xC:
+				return AKEY_i;
+			case 0xD:
+				return AKEY_j;
+			case 0xE:
+				return AKEY_k;
+			case 0xF:
+				return AKEY_l;
+			case 0x10:
+				return AKEY_m;
+			case 0x11:
+				return AKEY_n;
+			case 0x12:
+				return AKEY_o;
+			case 0x13:
+				return AKEY_p;
+			case 0x14:
+				return AKEY_q;
+			case 0x15:
+				return AKEY_r;
+			case 0x16:
+				return AKEY_s;
+			case 0x17:
+				return AKEY_t;
+			case 0x18:
+				return AKEY_u;
+			case 0x19:
+				return AKEY_v;
+			case 0x1A:
+				return AKEY_w;
+			case 0x1B:
+				return AKEY_x;
+			case 0x1C:
+				return AKEY_y;
+			case 0x1D:
+				return AKEY_z;
+			case 0x49:
+				return AKEY_INSERT_CHAR;
+			case 0x4C:     
+				return AKEY_DELETE_CHAR;
+			default:
+				break;
+			}
+		}
+		if((key.state == PS2KBD_RAWKEY_DOWN) && (PS2KbdCONTROL)){
+			switch(key.key){
+			case 0x4:
+				return AKEY_CTRL_a;
+			case 0x5:
+				return AKEY_CTRL_b;
+			case 0x6:
+				return AKEY_CTRL_c;
+			case 0x7:
+				return AKEY_CTRL_d;
+			case 0x8:
+				return AKEY_CTRL_e;
+			case 0x9:
+				return AKEY_CTRL_f;
+			case 0xA:
+				return AKEY_CTRL_g;
+			case 0xB:
+				return AKEY_CTRL_h;
+			case 0xC:
+				return AKEY_CTRL_i;
+			case 0xD:
+				return AKEY_CTRL_j;
+			case 0xE:
+				return AKEY_CTRL_k;
+			case 0xF:
+				return AKEY_CTRL_l;
+			case 0x10:
+				return AKEY_CTRL_m;
+			case 0x11:
+				return AKEY_CTRL_n;
+			case 0x12:
+				return AKEY_CTRL_o;
+			case 0x13:
+				return AKEY_CTRL_p;
+			case 0x14:
+				return AKEY_CTRL_q;
+			case 0x15:
+				return AKEY_CTRL_r;
+			case 0x16:
+				return AKEY_CTRL_s;
+			case 0x17:
+				return AKEY_CTRL_t;
+			case 0x18:
+				return AKEY_CTRL_u;
+			case 0x19:
+				return AKEY_CTRL_v;
+			case 0x1A:
+				return AKEY_CTRL_w;
+			case 0x1B:
+				return AKEY_CTRL_x;
+			case 0x1C:
+				return AKEY_CTRL_y;
+			case 0x1D:
+				return AKEY_CTRL_z;
+			case 0x2B:
+				return AKEY_CLRTAB;
+			default:
+				break;
+			}
+		}
+		if(key.state == PS2KBD_RAWKEY_UP){
+		switch (key.key){
+			case 0x39:
+			//todo: Turn off caps lock LED.
+			return AKEY_CAPSTOGGLE;                                
+			case 0xE0:
+				PS2KbdCONTROL=0;
+				return AKEY_NONE;
+			case 0xE4:
+				PS2KbdCONTROL=0;
+				return AKEY_NONE;
+			case 0xE1:
+				PS2KbdSHIFT=0;
+				return AKEY_NONE;
+			case 0xE5:
+				PS2KbdSHIFT=0;
+				return AKEY_NONE;
 			default:
 				break;
 			}
 		}
 	}
+}
 	return AKEY_NONE;
 }
 
@@ -436,13 +615,14 @@ int Atari_PORT(int num)
 
 int Atari_TRIG(int num)
 {
-	if (num == 0 && PadButtons() & PAD_SQUARE)
+	if (num == 0 && PadButtons() & PAD_CROSS)
 		return 0;
 	return 1;
 }
 
 int main(int argc, char **argv)
 {
+	loadModules();
 	/* initialise Atari800 core */
 	if (!Atari800_Initialise(&argc, argv))
 		return 3;
