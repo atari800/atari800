fffdafe52dab28f66c55f1c0fd78a98eb90c1378
Piotr Fusik <fox@scene.pl>
2005-10-19 21:34:34 +0000

    moved things from rt-config; removed Atari_DisplayScreen's argument

--- src/platform.h
+++ src/platform.h
@@ -1,24 +1,41 @@
 #ifndef _PLATFORM_H_
 #define _PLATFORM_H_
 
-#include "atari.h"
+#include "config.h"
+#include <stdio.h>
 
-/*
- * This include file defines prototypes for platforms specific functions.
- */
+/* This include file defines prototypes for platform-specific functions. */
 
 void Atari_Initialise(int *argc, char *argv[]);
 int Atari_Exit(int run_monitor);
 int Atari_Keyboard(void);
-void Atari_DisplayScreen (UBYTE *screen);
+void Atari_DisplayScreen(void);
 
 int Atari_PORT(int num);
 int Atari_TRIG(int num);
+
+#ifdef SUPPORTS_ATARI_CONFIGINIT
+/* This function sets the configuration parameters to default values */
+void Atari_ConfigInit(void);
+#endif
+
+#ifdef SUPPORTS_ATARI_CONFIGURE
+/* This procedure processes lines not recognized by RtConfigLoad. */
+int Atari_Configure(char *option, char *parameters);
+#endif
+
+#ifdef SUPPORTS_ATARI_CONFIGSAVE
+/* This function saves additional config lines */
+void Atari_ConfigSave(FILE *fp);
+#endif
 
 #endif /* _PLATFORM_H_ */
 
 /*
 $Log$
+Revision 1.7  2005/10/19 21:34:34  pfusik
+moved things from rt-config; removed Atari_DisplayScreen's argument
+
 Revision 1.6  2003/02/24 09:33:07  joy
 header cleanup
 
