d19f0b9a8575b0b87a94bd614bf2209c8169a264
Piotr Fusik <fox@scene.pl>
2005-10-19 21:44:07 +0000

    removed - full configuration available via User Interface

--- src/prompts.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PROMPTS__
-#define __PROMPTS__
-
-void GetString(char *message, char *string);
-void GetNumber(char *message, int *num);
-void GetYesNo(char *message, char *yn);
-void GetYesNoAsInt(char *message, int *num);
-
-#endif
