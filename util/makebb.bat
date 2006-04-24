0f15ad5dddf33f9e55a6980beedd3b03e29574e1
Piotr Fusik <fox@scene.pl>
2006-04-24 08:29:33 +0000

    removed (read cart.txt instead)

--- util/makebb.bat
+++
@@ -1,4 +0,0 @@
-@echo This program creates a bb.bin image
-@echo from bb_40_4f.bin, bb_50_5f.bin and bb_a0_bf.bin
-@echo for use with the Atari800 5200 emulator.
-copy /b bb_40_4f.bin + bb_50_5f.bin + bb_a0_bf.bin bb.bin
