fa65273887d20f37ffdceac8edcfab9abc6161b6
Petr Stehlík <petr@pstehlik.cz>
2010-12-07 14:15:44 +0000

    ver+

--- src/atari.h
+++ src/atari.h
@@ -9,7 +9,7 @@
 
 /* Fundamental declarations ---------------------------------------------- */
 
-#define Atari800_TITLE  "Atari 800 Emulator, Version 2.2.0rc1"
+#define Atari800_TITLE  "Atari 800 Emulator, Version 2.2.0rc2"
 
 #ifndef FALSE
 #define FALSE  0
