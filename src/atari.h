c6dea5b7fff354f90fd36decaec0ddb641221f39
Petr Stehlík <petr@pstehlik.cz>
2011-04-02 20:51:14 +0000

    updated for release

--- src/atari.h
+++ src/atari.h
@@ -9,7 +9,7 @@
 
 /* Fundamental declarations ---------------------------------------------- */
 
-#define Atari800_TITLE  "Atari 800 Emulator, Version 2.2.0rc2"
+#define Atari800_TITLE  "Atari 800 Emulator, Version 2.2.0"
 
 #ifndef FALSE
 #define FALSE  0
