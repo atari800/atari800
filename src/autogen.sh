d9e666fc085e547d05c0b053730dce102b262ef4
Tomasz Krasuski <kr0tki@poczta.onet.pl>
2013-01-27 23:55:07 +0000

    Rename the "basic" target to "default" and make it the default - the "--target" configure option can be omitted. Renamed the "--with-gui" configure option to "--with-video".

--- src/autogen.sh
+++ src/autogen.sh
@@ -4,11 +4,11 @@ autoheader
 autoconf
 
 echo
-echo "Now you need to run the configure script.  The configure script requires a"
-echo "\"--target=TARGET\" option, and it may also take various \"--enable-FEATURE\""
+echo "Now you need to run the configure script.  The configure script may take the"
+echo "\"--target=TARGET\" option and various \"--enable-FEATURE\" and \"--with-PACKAGE\""
 echo "options."
 echo
 echo "Run \"./configure --help\" to see all available options."
 echo "Run \"./configure --help=short\" just to see the Atari800 FEATURE options."
-echo "Run \"./configure\" without a parameter to see the valid targets."
+echo "Run \"./configure --target=help\" without a parameter to see the valid targets."
 echo
--- src/configure_dos.bat
+++ src/configure_dos.bat
@@ -1,1 +1,1 @@
-bash -c "CONFIG_SHELL=bash CC=gcc ./configure --target=basic --enable-bufferedlog %1 %2 %3 %4 %5 %6 %7 %8 %9"
+bash -c "CONFIG_SHELL=bash CC=gcc ./configure --enable-bufferedlog %1 %2 %3 %4 %5 %6 %7 %8 %9"
