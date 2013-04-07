0970854cb1a4024091230b6c31a278aded1917af
Tomasz Krasuski <kr0tki@poczta.onet.pl>
2013-04-07 02:57:18 +0000

    More accurate emulation of PAL colours based on analysis of oscillograms of PAL GTIA output.

--- src/colours_pal.c
+++ src/colours_pal.c
@@ -87,63 +87,223 @@ static void AdjustExternal(int colourtable[256])
 	}
 }
 
+/* Below are conclusions from analysis of oscillograms of PAL GTIA output.
+   The oscillograms are available at:
+   http://www.atari.org.pl/forum/viewtopic.php?pid=164037#p164037
+
+   Like the NTSC GTIA, the PAL GTIA produces color signal by delaying
+   (phase-shifting) a base chrominance subcarrier by a specified time -
+   different for each displayed hue. The subcarrier's frequency is
+   4433618.75 Hz (4.43 MHz in short). The subcarrier is provided at the chip's
+   PAL pin; output signal is produced at the COL pin.
+
+   As in NTSC, the delay introduced by the PAL GTIA can be adjusted to a
+   certain extent by changing voltage applied at the DEL pin.
+
+   This is where the similarities between the PAL GTIA and its NTSC
+   counterpart end.
+
+   In NTSC, each screen line contains, in its back porch, a burst of about 10
+   cycles of color subcarrier (known as the coloburst). The colorburst is used
+   by the TV as a reference for decoding colors in each line. Signal with
+   phase shift equal to the colorburst's (ie. equal to the subcarrier's) is
+   decoded as 180 deg angle in the YUV colorspace (-U). Delaying the signal is
+   interpreted as rotating the angle clockwise in YUV.
+
+   In PAL, there's also colorburst, but it is not equal to the color
+   subcarrier. In even lines it lags the subcarrier by 45 deg, and in odd
+   lines it leads it subcarrier by 45 deg. The resulting 90 deg difference is
+   used by the TV to distinguish even and odd lines. The original subcarrier
+   is recreated by the receiver by summing colorbursts from two consecutive
+   lines (and adjusting its amplitude, since sum of two sine waves shifted by
+   90 deg has higher amplitude than the original waves).
+
+   Then, as in NTSC, signals equal in phase to the (recreated) subcarrier are
+   decoded as 180 deg angle in YUV. But other signals are decoded differently
+   in even and in odd lines. In even lines (or "NTSC lines"), delaying the
+   signal is interpreted as rotating the angle clockwise in YUV, just like in
+   NTSC. But in odd lines (or "PAL lines"), delaying the signal is akin to
+   rotating the angle counter-clockwise. So, to achieve the same color in even
+   and odd lines, the transmitter must send signal delayed wrt. the subcarrier
+   in even lines but "rushed" (anti-delayed?) in odd lines.
+
+   PAL GTIA can only delay input signal; it can't "rush" it, it would involve
+   creating output before input has arrived. So the requirements of the PAL
+   system are achieved in the chip by delaying the signal differently in even
+   and odd lines.
+
+   Measurements indicate that the PAL GTIA delays the input subcarrier by
+   95.2ns; then it might delay it by further 100.7ns if needed; and finally
+   can delay it further by X, 2*X, 3*X, ..., 7*X, where X is a delay adjusted
+   by voltage at the DEL pin. The general formula is:
+     DELAY(hue) = 95.2ns + ADD(hue)*100.7ns + MULT(hue)*X
+   where
+     ADD(hue) depends on a given hue and can be 0 or 1,
+     MULT(hue) depends on a given hue and can be 0, 1, 2, ..., 7,
+     X depends on voltage at the DEL pin.
+   So, the chip can shift the input signal by 8*2=16 different delays.
+
+   Mapping of ADD and MULT values to specific hues is random - it is
+   implemented in the source code as the del_coeffs structure.
+
+   The relation between DEL pin voltage and the resulting delay is not linear
+   - it was measured to be:
+       17.37ns at 5V,
+       10.60ns at 7V,
+       7.44ns at 9V.
+
+   The PAL GTIA apparently contains a series of 7 delay elements similarly to
+   the NTSC version (which contains 15 of them), and an additional 100.7ns
+   delay element. (When looking at the oscillograms, for each hue that passes
+   through this 100.7ns delay, the resulting waveform is different than for
+   hues that don't involve the delay - it looks like it was inverted
+   vertically. This gives a hint about how the delay is implemented
+   internally.)
+
+   The end result is, PAL GTIA actually produces more than 256 colors. It
+   produces different hues in odd and even lines, but it is not normally
+   visible since PAL TVs average hues on each two consecutive lines - but
+   it can be made visible with specially-crafted screens. Currently this
+   feature of the chip is not implemented - only 256 colors are produced,
+   each one being an average of "even" and "odd" hues.
+
+   In PAL GTIA, just as in NTSC, the colorburst signals have the same phase as
+   Hue 1. But by changing voltage at the DEL pin, we also change the relative
+   phase shift of the colorburst between even and odd lines - it is not always
+   90 degrees. It has the following consequences - all of them observed on a
+   real machine, and all currently emulated:
+   1. Changing phase shift between odd and even colorbursts also changes
+   amplitude of the resulting subcarrier, which is recreated by summing the
+   two colorbursts. Since amplitude of the color subcarrier is used as a
+   reference for decoding color saturation, changing voltage at DEL not only
+   changes hues of displayed colors; it also changes their saturation.
+   2. With appropriately chosen DEL voltage, phase shift between even and odd
+   colorbursts can reach 180 deg. At this point the PAL TV can no longer
+   distinguish even and odd lines, and stops interpreting color signal,
+   displaying screen as black-and-white with visible chroma dots.
+   3. Changing DEL voltage further causes phase shift between even and odd
+   colorbursts to to be larger than 180 deg, at which point the TV starts
+   interpreting color signal "backwards", as if the color subcarrier suddenly
+   shifted by 180 deg.
+*/
+
+/* When phase shift between even and odd colorbursts is close to 180 deg, the
+   TV stops interpreting color signal. This value determines how close to 180
+   deg that phase shift must be. It is specific to a TV set. */
+#define COLOR_DISABLE_THRESHOLD 0.05
+
 /* Generates a PAL palette, based on colour setup. Result is written into
-   COLOURTABLE.
-   PAL GTIA produces slightly different hues on odd and even lines;
-   they are later averaged in the TV (a feature of PAL system). The generated
-   palette reproduces the real machine somewhat - each hue is an average of
-   an "odd" hue and an "even" hue. */
+   COLOURTABLE. */
 static void GeneratePalette(int colourtable[256])
 {
+	struct del_coeff {
+		int add;
+		int mult;
+	};
+
+	/* Delay coefficients for each hue. */
+	static struct {
+		struct del_coeff even[15];
+		struct del_coeff odd[15];
+	} del_coeffs = {
+		{ { 1, 5 }, /* Hue $1 in even lines */
+		  { 1, 6 }, /* Hue $2 in even lines */
+		  { 1, 7 },
+		  { 0, 0 },
+		  { 0, 1 },
+		  { 0, 2 },
+		  { 0, 4 },
+		  { 0, 5 },
+		  { 0, 6 },
+		  { 0, 7 },
+		  { 1, 1 },
+		  { 1, 2 },
+		  { 1, 3 },
+		  { 1, 4 },
+		  { 1, 5 } /* Hue $F in even lines */
+		},
+		{ { 1, 1 }, /* Hue $1 in odd lines */
+		  { 1, 0 }, /* Hue $2 in odd lines */
+		  { 0, 7 },
+		  { 0, 6 },
+		  { 0, 5 },
+		  { 0, 4 },
+		  { 0, 2 },
+		  { 0, 1 },
+		  { 0, 0 },
+		  { 1, 7 },
+		  { 1, 5 },
+		  { 1, 4 },
+		  { 1, 3 },
+		  { 1, 2 },
+		  { 1, 1 } /* Hue $F in odd lines */
+		}
+};
 	int cr, lm;
 
-	double scaled_black_level = (double)COLOURS_PAL_setup.black_level / 255.0f;
-	double scaled_white_level = (double)COLOURS_PAL_setup.white_level / 255.0f;
-	const double gamma = 1 - COLOURS_PAL_setup.gamma / 2.0;
-
-	double const start_angle = COLOURS_PAL_setup.hue * M_PI;
-	double color_diff = COLOURS_PAL_setup.color_delay * M_PI / 180.0;
+	double const scaled_black_level = (double)COLOURS_PAL_setup.black_level / 255.0f;
+	double const scaled_white_level = (double)COLOURS_PAL_setup.white_level / 255.0f;
+	double const gamma = 1 - COLOURS_PAL_setup.gamma / 2.0;
 
 	/* NTSC luma multipliers from CGIA.PDF */
-	double luma_mult[16] = {
+	static double const luma_mult[16] = {
 		0.6941, 0.7091, 0.7241, 0.7401,
 		0.7560, 0.7741, 0.7931, 0.8121,
 		0.8260, 0.8470, 0.8700, 0.8930,
 		0.9160, 0.9420, 0.9690, 1.0000};
 
+	/* Base delay - 1/4.43MHz * base_del = ca. 95.2ns */
+	static double const base_del = 0.421894970414201;
+	/* Additional delay - 1/4.43MHz * add_del = ca. 100.7ns */
+	static double const add_del = 0.446563064859117;
+	/* Delay introduced by the DEL pin voltage. */
+	double const del_adj = COLOURS_PAL_setup.color_delay / 360.0;
+
+	/* Phase delays of colorbursts in even and odd lines. They are equal to
+	   Hue 1. */
+	double const even_burst_del = base_del + add_del * del_coeffs.even[0].add + del_adj * del_coeffs.even[0].mult;
+	double const odd_burst_del = base_del + add_del * del_coeffs.odd[0].add + del_adj * del_coeffs.odd[0].mult;
+
+	/* Reciprocal of the recreated subcarrier's amplitude. */
+	double saturation_mult;
+	/* Phase delay of the recreated amplitude. */
+	double subcarrier_del = (even_burst_del + odd_burst_del + COLOURS_PAL_setup.hue) / 2.0;
+
+	/* Phase difference between colorbursts in even and odd lines. */
+	double burst_diff = even_burst_del - odd_burst_del;
+	burst_diff -= floor(burst_diff); /* Normalize to 0..1. */
+
+	if (burst_diff > 0.5 - COLOR_DISABLE_THRESHOLD && burst_diff < 0.5 + COLOR_DISABLE_THRESHOLD)
+		/* Shift between colorbursts close to 180 deg. Don't produce color. */
+		saturation_mult = 0.0;
+	else {
+		/* Subcarrier is a sum of two waves with equal frequency and amplitude,
+		   but phase-shifted by 2pi*burst_diff. The formula is derived from
+		   http://2000clicks.com/mathhelp/GeometryTrigEquivPhaseShift.aspx */
+		double subcarrier_amplitude = sqrt(2.0 * cos(burst_diff*2.0*M_PI) + 2.0);
+		/* Normalise saturation_mult by multiplying by sqrt(2), so that it
+		  equals 1.0 when odd & even colorbursts are shifted by 90 deg (ie.
+		  burst_diff == 0.25). */
+		saturation_mult = sqrt(2.0) / subcarrier_amplitude;
+	}
+
 	for (cr = 0; cr < 16; cr ++) {
 		double u = 0.0;
 		double v = 0.0;
 		if (cr) {
-			struct pal_phase {
-				double mult;
-				double shift;
-			};
-			static struct pal_phase const phases[14] = {
-					{  1.5, M_PI },
-					{  2.5, M_PI },
-					{  3.5, M_PI },
-					{ -1.5,  0.0 },
-					{ -0.5,  0.0 },
-					{  0.0,  0.0 },
-					{  1.5,  0.0 },
-					{  2.5,  0.0 },
-					{  3.5,  0.0 },
-					{  4.0,  0.0 },
-					{ -2.5, M_PI },
-					{ -1.5, M_PI },
-					{ -0.5, M_PI },
-					{  0.5, M_PI }
-			};
-			struct pal_phase const *odd_phase = &phases[(cr - 1) % 14];
-			struct pal_phase const *even_phase = &phases[(25 -(cr - 1)) % 14];
-			double odd_angle = start_angle + odd_phase->shift - color_diff * (odd_phase->mult);
-			double even_angle = start_angle + even_phase->shift + color_diff * (even_phase->mult - 0.5);
-			double odd_u = cos(odd_angle);
-			double odd_v = sin(odd_angle);
+			struct del_coeff const *even_delay = &(del_coeffs.even[cr - 1]);
+			struct del_coeff const *odd_delay = &(del_coeffs.odd[cr - 1]);
+			double even_del = base_del + add_del * even_delay->add + del_adj * even_delay->mult;
+			double odd_del = base_del + add_del * odd_delay->add + del_adj * odd_delay->mult;
+			double even_angle = (0.5 - (even_del - subcarrier_del)) * 2.0 * M_PI;
+			double odd_angle = (0.5 + (odd_del - subcarrier_del)) * 2.0 * M_PI;
 			double even_u = cos(even_angle);
 			double even_v = sin(even_angle);
-			double saturation = (COLOURS_PAL_setup.saturation + 1) * 0.175;
+			double odd_u = cos(odd_angle);
+			double odd_v = sin(odd_angle);
+			double saturation = (COLOURS_PAL_setup.saturation + 1) * 0.175 * saturation_mult;
+			/* TODO: the different colors in odd and even lines are not
+			   emulated - instead the palette contains averaged values. */
 			u = (even_u + odd_u) * 0.5 * saturation;
 			v = (even_v + odd_v) * 0.5 * saturation;
 		}
