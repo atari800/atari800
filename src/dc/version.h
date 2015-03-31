186b3e484a479b960c8295aa7719a60692b49032
Christian Groessler <chris@groessler.org>
2015-03-31 22:00:23 +0000

    better "BETA version" handling

--- src/dc/version.h
+++ src/dc/version.h
@@ -3,12 +3,15 @@
 #ifndef __VERSIONDC_H_
 #define __VERSIONDC_H_
 
-#define A800DCBETA    1      /* set to 1 for beta versions, to 0 for release versions */
+#define xstr(s) str(s) /* taken from gcc docs */
+#define str(s) #s      /*   "  "  "  "  "  "  */
+
+#define A800DCBETA    1      /* set to 1 (or more to differentiate) for beta versions, to 0 for release versions */
 #define A800DCVERHI   0
 #define A800DCVERLO   79
 #define A800DCVERASC1 "0.79"
 #if A800DCBETA
-#define A800DCVERASC  A800DCVERASC1 "beta"
+#define A800DCVERASC  A800DCVERASC1 "beta" xstr(A800DCBETA)
 #else
 #define A800DCVERASC  A800DCVERASC1
 #endif
