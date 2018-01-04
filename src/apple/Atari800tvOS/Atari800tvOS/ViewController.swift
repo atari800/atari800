0e024ecb8a7b6b9cd397d918c241036251b3c0d1
Rod Münch <rod.munch@behindthefridge.co.uk>
2018-01-04 19:21:04 +0000

    Add files for tvOS project

---
+++ src/apple/Atari800tvOS/Atari800tvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Atari800tvOS
+//
+//  Created by Rod Münch on 04/01/2018.
+//  Copyright © 2018 Atari800 development team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
