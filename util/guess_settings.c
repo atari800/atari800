5c53d78b1164893f51c26a64cd88143ee60587dc
Rob McMullen <feedback@playermissile.com>
2019-01-10 19:30:19 -0800

    Added cartridge type testing

--- util/guess_settings.c
+++ util/guess_settings.c
@@ -11,30 +11,27 @@ typedef struct {
 } machine_config_t;
 
 machine_config_t machine_config[] = {
-	"400/800   NTSC  OS/B", FALSE, 400, {"-atari", "-ntsc", NULL},
-	"400/800   PAL   OS/B", FALSE, 400, {"-atari", "-pal", NULL},
-	"400/800   NTSC  Altirra", FALSE, 400, {"-atari", "-ntsc", "-800-rev", "altirra", NULL},
-	"400/800   PAL   Altirra", FALSE, 400, {"-atari", "-pal", "-800-rev", "altirra", NULL},
-
-	"64k XL    NTSC  XL ROM", FALSE, 400, {"-xl", "-ntsc", NULL},
-	"64k XL    PAL   XL ROM", FALSE, 400, {"-xl", "-pal", NULL},
-	"64k XL    NTSC  Altirra", FALSE, 400, {"-xl", "-ntsc", "-xl-rev", "altirra", NULL},
-	"64k XL    PAL   Altirra", FALSE, 400, {"-xl", "-pal", "-xl-rev", "altirra", NULL},
-
-	"128k XE   NTSC  XL ROM", FALSE, 400, {"-xe", "-ntsc", NULL},
-	"128k XE   PAL   XL ROM", FALSE, 400, {"-xe", "-pal", NULL},
-	"128k XE   NTSC  Altirra", FALSE, 400, {"-xe", "-ntsc", "-xl-rev", "altirra", NULL},
-	"128k XE   PAL   Altirra", FALSE, 400, {"-xe", "-pal", "-xl-rev", "altirra", NULL},
-
-	"5200      NTSC  5200 ROM", TRUE, 400, {"-5200", "-ntsc", NULL},
-	"5200      NTSC  Altirra", TRUE, 6000, {"-5200", "-ntsc", "-5200-rev", "altirra", NULL},
+	"400/800 NTSC OS/B   ", FALSE, 400, {"-atari", "-ntsc", NULL},
+	"400/800 PAL  OS/B   ", FALSE, 400, {"-atari", "-pal", NULL},
+	"400/800 NTSC Altirra", FALSE, 400, {"-atari", "-ntsc", "-800-rev", "altirra", NULL},
+	"400/800 PAL  Altirra", FALSE, 400, {"-atari", "-pal", "-800-rev", "altirra", NULL},
+	"64k XL  NTSC XL ROM ", FALSE, 400, {"-xl", "-ntsc", NULL},
+	"64k XL  PAL  XL ROM ", FALSE, 400, {"-xl", "-pal", NULL},
+	"64k XL  NTSC Altirra", FALSE, 400, {"-xl", "-ntsc", "-xl-rev", "altirra", NULL},
+	"64k XL  PAL  Altirra", FALSE, 400, {"-xl", "-pal", "-xl-rev", "altirra", NULL},
+	"128k XE NTSC XL ROM ", FALSE, 400, {"-xe", "-ntsc", NULL},
+	"128k XE PAL  XL ROM ", FALSE, 400, {"-xe", "-pal", NULL},
+	"128k XE NTSC Altirra", FALSE, 400, {"-xe", "-ntsc", "-xl-rev", "altirra", NULL},
+	"128k XE PAL  Altirra", FALSE, 400, {"-xe", "-pal", "-xl-rev", "altirra", NULL},
+	"5200    NTSC Atari  ", TRUE, 400, {"-5200", "-ntsc", NULL},
+	"5200    NTSC Altirra", TRUE, 6000, {"-5200", "-ntsc", "-5200-rev", "altirra", NULL},
 	NULL, FALSE, 400, {NULL},
 };
 
 typedef struct {
 	int size;
 	int type;
-	char *name;
+	char *label;
 } cart_types_t;
 
 cart_types_t cart_list_a8[] = {
@@ -99,6 +96,7 @@ cart_types_t cart_list_a8[] = {
 	{32768,65,"The!Cart 32 MB",},
 	{65536,66,"The!Cart 64 MB",},
 	{131072,62,"The!Cart 128 MB",},
+	{0, 0, NULL},
 };
 
 cart_types_t cart_list_5200[] = {
@@ -108,6 +106,7 @@ cart_types_t cart_list_5200[] = {
 	{16, 6,  "Two chip 16 KB 5200",},
 	{32, 4,  "Standard 32 KB 5200",},
 	{40, 7,  "Bounty Bob 40 KB 5200",},
+	{0, 0, NULL},
 };
 
 char *test_args[32];
@@ -134,18 +133,109 @@ int run_emulator(int num_args, int num_frames, int verbose) {
 	input_template_t input;
 
 	int frame = 0;
-	int success = 1;
 	while (frame < num_frames) {
 		// libatari800_get_current_state(state, &tags);
 		if (!libatari800_next_frame(&input)) {
 			printf("Crash encountered at frame %d\n", frame);
-			success = 0;
+			frame = -frame;
 			break;
 		}
 		frame++;
 	}
 
-	return success;
+	return frame;
+}
+
+cart_types_t *get_first_cart(machine_config_t *machine, int cart_kb) {
+	cart_types_t *cart_desc = NULL;
+
+	if (cart_kb > 0) {
+		if (machine->is_5200) cart_desc = cart_list_5200;
+		else cart_desc = cart_list_a8;
+
+		while (cart_desc->size < cart_kb) cart_desc++;
+	}
+	return cart_desc;
+}
+
+char cart_type_string[16];
+
+void run_machine(machine_config_t *machine, char *pathname, int num_frames, int cart_kb, int verbose) {
+	int success;
+	char **machine_args;
+	cart_types_t *cart_desc = get_first_cart(machine, cart_kb);
+
+	num_frames = num_frames < machine->min_frames ? machine->min_frames : num_frames;
+	while (1) {
+		/* args array is modified by atari800, so need to recreate it each time */
+		int num_args = 0;
+		while (num_args < (sizeof(default_args) / sizeof(default_args[0]))) {
+			test_args[num_args] = default_args[num_args];
+			// printf("default arg %d: %s\n", num_args, default_args[num_args]);
+			num_args++;
+		}
+		machine_args = machine->args;
+		while (*machine_args) {
+			// printf("arg %d: %s\n", num_args, *machine_args);
+			test_args[num_args++] = *machine_args++;
+		}
+		if (cart_desc && cart_desc->size == cart_kb) {
+			test_args[num_args++] = "-cart-type";
+			sprintf(cart_type_string, "%d", cart_desc->type);
+			test_args[num_args++] = cart_type_string;
+			test_args[num_args++] = "-cart";
+		}
+		test_args[num_args++] = pathname;
+
+		success = run_emulator(num_args, num_frames, verbose);
+		printf("%s: %s", pathname, machine->label);
+		if (cart_desc) {
+			printf(" (cart=%d '%s')", cart_desc->type, cart_desc->label);
+		}
+		if (success > 0) printf(" boot: OK through %d frames\n", success);
+		else printf(" boot: FAIL\n");
+
+		if (!cart_desc) break;
+		if (cart_desc) {
+			cart_desc++;
+			if (cart_desc->size != cart_kb) cart_desc = NULL;
+		}
+	}
+}
+
+#define CHUNK_SIZE 1024
+
+int guess_cart_kb(char *pathname) {
+	char buf[CHUNK_SIZE];
+	FILE *fp = fopen(pathname, "rb");
+	size_t current_len, total_len = 0;
+	int kb;
+	cart_types_t *cart_desc;
+
+	do {
+		current_len = fread(buf, 1, CHUNK_SIZE, fp);
+		total_len += current_len;
+	} while (current_len == CHUNK_SIZE);
+
+	kb = (int)(total_len / 2048) * 2048; /* 2k smallest cart */
+	int found = FALSE;
+	if (total_len == kb) {
+		kb /= 1024;
+		cart_desc = cart_list_a8;
+		while (cart_desc->size) {
+			if (kb == cart_desc->size) {
+				found = TRUE;
+				break;
+			}
+			cart_desc++;
+		}
+		printf("%s: possible cartridge; size=%dkb\n", pathname, kb);
+	}
+	if (!found) {
+		printf("%s: not cartridge; %d bytes\n", pathname, (int)total_len);
+		kb = -1;
+	}
+	return kb;
 }
 
 int main(int argc, char **argv) {
@@ -165,27 +255,11 @@ int main(int argc, char **argv) {
 			}
 		}
 		else {
-			char **machine_args;
 			int machine_index, success;
 			machine_config_t *machine = machine_config;
+			int cart_kb = guess_cart_kb(argv[i]);
 			while (machine->label) {
-				/* args array is modified by atari800, so need to recreate it each time */
-				int num_args = 0;
-				while (num_args < (sizeof(default_args) / sizeof(default_args[0]))) {
-					test_args[num_args] = default_args[num_args];
-					// printf("default arg %d: %s\n", num_args, default_args[num_args]);
-					num_args++;
-				}
-				machine_args = machine->args;
-				while (*machine_args) {
-					// printf("arg %d: %s\n", num_args, *machine_args);
-					test_args[num_args++] = *machine_args++;
-				}
-				test_args[num_args++] = argv[i];
-				success = run_emulator(num_args, num_frames < machine->min_frames ? machine->min_frames : num_frames, verbose);
-				printf("%s: %s  Emulation ", argv[i], machine->label);
-				if (success) printf("succeeded.\n");
-				else printf("failed.\n");
+				run_machine(machine, argv[i], num_frames, cart_kb, verbose);
 				machine++;
 			}
 		}
