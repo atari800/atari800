atari800/atari800

commit 5f6d891ac1023917133c2ec560dad3eaa27e90d7

Fixed computation of gamma adjustment - it is now applied to each of the three RGB channels separately.

src/atari_ntsc/atari_ntsc.c
@@ -1,5 +1,6 @@
 /* Based on nes_ntsc 0.2.2. http://www.slack.net/~ant/ */
 
+#include "colours.h"
 #include "atari_ntsc.h"
 
 /* Copyright (C) 2006-2007 Shay Green. This module is free software; you
@@ -94,6 +95,23 @@ void atari_ntsc_init( atari_ntsc_t* ntsc, atari_ntsc_setup_t const* setup )
 			y = *yiq_ptr++;
 			i = *yiq_ptr++;
 			q = *yiq_ptr++;
+		}
+
+		/* Atari change: Convert from CRT TV gamma correction to the sRGB
+		   gamma correction. */
+		if (setup->gamma >= 0.0)
+		{
+			double r, g, b = YIQ_TO_RGB( y, i, q, default_decoder, double, r, g );
+
+			r = Colours_Gamma2Linear(r, setup->gamma);
+			g = Colours_Gamma2Linear(g, setup->gamma);
+			b = Colours_Gamma2Linear(b, setup->gamma);
+
+			r = Colours_Linear2sRGB(r);
+			g = Colours_Linear2sRGB(g);
+			b = Colours_Linear2sRGB(b);
+
+			q = RGB_TO_YIQ( r, g, b, y, i );
 		}
 
 		i *= rgb_unit;

src/colours.c
@@ -45,11 +45,17 @@
 Colours_setup_t *Colours_setup;
 COLOURS_EXTERNAL_t *Colours_external;
 
+/* The NTSC and PAL TV systems maintain that the gamma factor of CRT TV
+   receivers should be 2.5 and 2.8, respectively. However, typical CRT TVs
+   (both NTSC and PAL) would have the gamma factor somewhere in the range
+   between 2.35 and 2.55. Therefore we choose 2.35 as the default gamma for
+   both TV systems. Reference: sections 10, 11 and 19 of
+   http://www.poynton.com/notes/colour_and_gamma/GammaFAQ.html */
 static Colours_setup_t const presets[] = {
 	/* Hue, Saturation, Contrast, Brightness, Gamma adjustment, GTIA delay, Black level, White level */
-	{ 0.0, 0.0, 0.0, 0.0, 0.3, 0.0, 16, 235 }, /* Standard preset */
-	{ 0.0, 0.0, 0.2, -0.16, 0.5, 0.0, 16, 235 }, /* Deep blacks preset */
-	{ 0.0, 0.26, 0.72, -0.16, 0.16, 0.0, 16, 235 } /* Vibrant colours & levels preset */
+	{ 0.0, 0.0, 0.0, 0.0, 2.35, 0.0, 16, 235 }, /* Standard preset */
+	{ 0.0, 0.0, 0.08, -0.08, 2.35, 0.0, 16, 235 }, /* Deep blacks preset */
+	{ 0.0, 0.26, 0.72, -0.16, 2.00, 0.0, 16, 235 } /* Vibrant colours & levels preset */
 };
 static char const * const preset_cfg_strings[COLOURS_PRESET_SIZE] = {
 	"STANDARD",
@@ -106,6 +112,26 @@ void Colours_RGB2YUV(double r, double g, double b, double *y, double *u, double 
 void Colours_YUV2RGB(double y, double u, double v, double *r, double *g, double *b)
 {
 	MultiplyMatrix(y, u, v, r, g, b, YUV2RGB_matrix);
+}
+
+double Colours_Gamma2Linear(double c, double gamma_adj)
+{
+	if (c >= 0.0)
+		return pow(c, gamma_adj);
+	else
+		/* Can't do pow() on a negative c. Let's divide c by the same value as
+		   in Colours_Linear2sRGB, so that a later conversion to sRGB will
+		   result in the value of c being unchanged. */
+		return c / 12.92;
+}
+
+double Colours_Linear2sRGB(double c)
+{
+	/* Source: https://en.wikipedia.org/wiki/SRGB */
+	if (c <= 0.0031308)
+		return c * 12.92;
+	else
+		return 1.055 * pow(c, 1.0/2.4) - 0.055;
 }
 
 static void UpdateModeDependentPointers(int tv_mode)

src/colours.h
@@ -39,8 +39,8 @@ typedef struct Colours_setup_t {
 #define COLOURS_CONTRAST_MAX 2.0
 #define COLOURS_BRIGHTNESS_MIN -2.0
 #define COLOURS_BRIGHTNESS_MAX 2.0
-#define COLOURS_GAMMA_MIN -1.0
-#define COLOURS_GAMMA_MAX 1.0
+#define COLOURS_GAMMA_MIN 1.0
+#define COLOURS_GAMMA_MAX 3.5
 #define COLOURS_DELAY_MIN 10
 #define COLOURS_DELAY_MAX 50
 
@@ -95,5 +95,11 @@ void Colours_RGB2YUV(double r, double g, double b, double *y, double *u, double 
 /* Convert given Y, U and V values to corresponding R, G, B values (all in the
    0.0 - 1.0 range). */
 void Colours_YUV2RGB(double y, double u, double v, double *r, double *g, double *b);
+
+/* Converts a gamma-adjusted color value c (0 <= c <= 1) into linear value. */
+double Colours_Gamma2Linear(double c, double gamma_adj);
+/* Converts a linear color value c (0 <= c <= 1) into sRGB gamma-corrected
+   value. */
+double Colours_Linear2sRGB(double c);
 
 #endif /* COLOURS_H_ */

src/colours_ntsc.c
@@ -68,7 +68,6 @@ static const double colorburst_angle = (303.0f) * M_PI / 180.0f;
    External palette is not adjusted if COLOURS_NTSC_external.adjust is false. */
 static void UpdateYIQTableFromExternal(double yiq_table[768], double start_angle, const double start_saturation)
 {
-	const double gamma = 1 - COLOURS_NTSC_setup.gamma / 2.0;
 	unsigned char *ext_ptr = COLOURS_NTSC_external.palette;
 	int n;
 
@@ -89,7 +88,6 @@ static void UpdateYIQTableFromExternal(double yiq_table[768], double start_angle
 		q = tmp_i * s + q * c;
 		/* Optionally adjust external palette. */
 		if (COLOURS_NTSC_external.adjust) {
-			y = pow(y, gamma);
 			y *= COLOURS_NTSC_setup.contrast * 0.5 + 1;
 			y += COLOURS_NTSC_setup.brightness * 0.5;
 			if (y > 1.0)
@@ -118,7 +116,6 @@ static void UpdateYIQTableFromGenerated(double yiq_table[768], const double star
 
 	double scaled_black_level = (double)COLOURS_NTSC_setup.black_level / 255.0;
 	double scaled_white_level = (double)COLOURS_NTSC_setup.white_level / 255.0;
-	const double gamma = 1 - COLOURS_NTSC_setup.gamma / 2.0;
 
 	/* NTSC luma multipliers from CGIA.PDF */
 	double luma_mult[16]={
@@ -136,7 +133,6 @@ static void UpdateYIQTableFromGenerated(double yiq_table[768], const double star
 		for (lm = 0; lm < 16; lm ++) {
 			/* calculate yiq for color entry */
 			double y = (luma_mult[lm] - luma_mult[0]) / (luma_mult[15] - luma_mult[0]);
-			y = pow(y, gamma);
 			y *= COLOURS_NTSC_setup.contrast * 0.5 + 1;
 			y += COLOURS_NTSC_setup.brightness * 0.5;
 			/* Scale the Y signal's range from 0..1 to
@@ -186,6 +182,20 @@ static void YIQ2RGB(int colourtable[256], const double yiq_table[768])
 		r = y + 0.9563 * i + 0.6210 * q;
 		g = y - 0.2721 * i - 0.6474 * q;
 		b = y - 1.1070 * i + 1.7046 * q;
+
+		if (!COLOURS_NTSC_external.loaded || COLOURS_NTSC_external.adjust) {
+			/* The r, g, b values derived from the YIQ signal are non-linear
+			   (ie. gamma-corrected). We convert them to linear values,
+			   assuming the CRT TV's gamma = COLOURS_NTSC_setup.gamma. */
+			r = Colours_Gamma2Linear(r, COLOURS_NTSC_setup.gamma);
+			g = Colours_Gamma2Linear(g, COLOURS_NTSC_setup.gamma);
+			b = Colours_Gamma2Linear(b, COLOURS_NTSC_setup.gamma);
+			/* Now we convert the linear values to the sRGB colourspace. */
+			r = Colours_Linear2sRGB(r);
+			g = Colours_Linear2sRGB(g);
+			b = Colours_Linear2sRGB(b);
+		}
+
 		Colours_SetRGB(n, (int) (r * 255), (int) (g * 255), (int) (b * 255), colourtable);
 	}
 }

src/colours_pal.c
@@ -50,7 +50,6 @@ COLOURS_EXTERNAL_t COLOURS_PAL_external = { "", FALSE, FALSE };
    COLOURS_PAL_external.adjust is false. */
 static void GetYUVFromExternal(double yuv_table[256*5])
 {
-	double const gamma = 1 - COLOURS_PAL_setup.gamma / 2.0;
 	unsigned char *ext_ptr = COLOURS_PAL_external.palette;
 	int n;
 
@@ -70,7 +69,6 @@ static void GetYUVFromExternal(double yuv_table[256*5])
 		v = tmp_u * s + v * c;
 		/* Optionally adjust external palette. */
 		if (COLOURS_PAL_external.adjust) {
-			y = pow(y, gamma);
 			y *= COLOURS_PAL_setup.contrast * 0.5 + 1;
 			y += COLOURS_PAL_setup.brightness * 0.5;
 			if (y > 1.0)
@@ -241,7 +239,6 @@ static void GetYUVFromGenerated(double yuv_table[256*5])
 
 	double const scaled_black_level = (double)COLOURS_PAL_setup.black_level / 255.0f;
 	double const scaled_white_level = (double)COLOURS_PAL_setup.white_level / 255.0f;
-	double const gamma = 1 - COLOURS_PAL_setup.gamma / 2.0;
 
 	/* NTSC luma multipliers from CGIA.PDF */
 	static double const luma_mult[16] = {
@@ -310,7 +307,6 @@ static void GetYUVFromGenerated(double yuv_table[256*5])
 		for (lm = 0; lm < 16; lm ++) {
 			/* calculate yuv for color entry */
 			double y = (luma_mult[lm] - luma_mult[0]) / (luma_mult[15] - luma_mult[0]);
-			y = pow(y, gamma);
 			y *= COLOURS_PAL_setup.contrast * 0.5 + 1;
 			y += COLOURS_PAL_setup.brightness * 0.5;
 			/* Scale the Y signal's range from 0..1 to
@@ -357,6 +353,20 @@ static void YUV2RGB(int colourtable[256], double const yuv_table[256*5])
 		double u = (even_u + odd_u) / 2.0;
 		double v = (even_v + odd_v) / 2.0;
 		Colours_YUV2RGB(y, u, v, &r, &g, &b);
+
+		if (!COLOURS_PAL_external.loaded || COLOURS_PAL_external.adjust) {
+			/* The r, g, b values derived from the YUV signal are non-linear
+			   (ie. gamma-corrected). We convert them to linear values,
+			   assuming the CRT TV's gamma = COLOURS_PAL_setup.gamma. */
+			r = Colours_Gamma2Linear(r, COLOURS_PAL_setup.gamma);
+			g = Colours_Gamma2Linear(g, COLOURS_PAL_setup.gamma);
+			b = Colours_Gamma2Linear(b, COLOURS_PAL_setup.gamma);
+			/* Now we convert the linear values to the sRGB colourspace. */
+			r = Colours_Linear2sRGB(r);
+			g = Colours_Linear2sRGB(g);
+			b = Colours_Linear2sRGB(b);
+		}
+
 		Colours_SetRGB(n, (int) (r * 255), (int) (g * 255), (int) (b * 255), colourtable);
 	}
 }

src/filter_ntsc.c
@@ -77,6 +77,7 @@ void FILTER_NTSC_Update(atari_ntsc_t *filter)
 		FILTER_NTSC_setup.saturation = 0.0;
 		FILTER_NTSC_setup.contrast = 0.0;
 		FILTER_NTSC_setup.brightness = 0.0;
+		FILTER_NTSC_setup.gamma = -1.0; /* indicates no gamma correction shall be done */
 	} else {
 		/* It's either generated palette, or external palette with
 		   need for adjustments. Copy values from
@@ -85,6 +86,7 @@ void FILTER_NTSC_Update(atari_ntsc_t *filter)
 		FILTER_NTSC_setup.saturation = COLOURS_NTSC_setup.saturation;
 		FILTER_NTSC_setup.contrast = COLOURS_NTSC_setup.contrast;
 		FILTER_NTSC_setup.brightness = COLOURS_NTSC_setup.brightness;
+		FILTER_NTSC_setup.gamma = COLOURS_NTSC_setup.gamma;
 	}
 
 	FILTER_NTSC_setup.yiq_palette = yiq_table;

src/pal_blending.c
@@ -62,8 +62,24 @@ void PAL_BLENDING_UpdateLookup(void)
 			double odd_v = *ptr++;
 			double r, g, b;
 			Colours_YUV2RGB(y, even_u, even_v, &r, &g, &b);
+			if (!COLOURS_PAL_external.loaded || COLOURS_PAL_external.adjust) {
+				r = Colours_Gamma2Linear(r, COLOURS_PAL_setup.gamma);
+				g = Colours_Gamma2Linear(g, COLOURS_PAL_setup.gamma);
+				b = Colours_Gamma2Linear(b, COLOURS_PAL_setup.gamma);
+				r = Colours_Linear2sRGB(r);
+				g = Colours_Linear2sRGB(g);
+				b = Colours_Linear2sRGB(b);
+			}
 			Colours_SetRGB(i, (int) (r * 255), (int) (g * 255), (int) (b * 255), even_pal);
 			Colours_YUV2RGB(y, odd_u, odd_v, &r, &g, &b);
+			if (!COLOURS_PAL_external.loaded || COLOURS_PAL_external.adjust) {
+				r = Colours_Gamma2Linear(r, COLOURS_PAL_setup.gamma);
+				g = Colours_Gamma2Linear(g, COLOURS_PAL_setup.gamma);
+				b = Colours_Gamma2Linear(b, COLOURS_PAL_setup.gamma);
+				r = Colours_Linear2sRGB(r);
+				g = Colours_Linear2sRGB(g);
+				b = Colours_Linear2sRGB(b);
+			}
 			Colours_SetRGB(i, (int) (r * 255), (int) (g * 255), (int) (b * 255), odd_pal);
 		}
 		PLATFORM_GetPixelFormat(&format);

commit ea1be661f940ca031749f7f620e05fe67ef4478d

Update gamma values in NTSC filter presets to the new values.
This fixes issue #89, introduced in commit 5f6d891ac1023917133c2ec560dad3eaa27e90d7

src/atari_ntsc/atari_ntsc.c
@@ -16,10 +16,10 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA */
 
 /* Atari change: removal and addition of structure fields.
    Values of resolution and sharpness adjusted to make NTSC artifacts look better. */
-atari_ntsc_setup_t const atari_ntsc_monochrome = { 0, -1, 0, 0, -.3,  .3,  .2, -.2, -.2, -1, 0, 0, 0, 0. };
-atari_ntsc_setup_t const atari_ntsc_composite  = { 0,  0, 0, 0, -.5,  .3, -.1,   0,   0,  0, 0, 0, 0, 0. };
-atari_ntsc_setup_t const atari_ntsc_svideo     = { 0,  0, 0, 0, -.3,  .3,  .2,  -1,  -1,  0, 0, 0, 0, 0. };
-atari_ntsc_setup_t const atari_ntsc_rgb        = { 0,  0, 0, 0, -.3,  .3,  .7,  -1,  -1, -1, 0, 0, 0, 0. };
+atari_ntsc_setup_t const atari_ntsc_monochrome = { 0, -1, 0, 0, -.3,  2.35,  .2, -.2, -.2, -1, 0, 0, 0, 0. };
+atari_ntsc_setup_t const atari_ntsc_composite  = { 0,  0, 0, 0, -.5,  2.35, -.1,   0,   0,  0, 0, 0, 0, 0. };
+atari_ntsc_setup_t const atari_ntsc_svideo     = { 0,  0, 0, 0, -.3,  2.35,  .2,  -1,  -1,  0, 0, 0, 0, 0. };
+atari_ntsc_setup_t const atari_ntsc_rgb        = { 0,  0, 0, 0, -.3,  2.35,  .7,  -1,  -1, -1, 0, 0, 0, 0. };
 
 #define alignment_count 4
 #define burst_count     1