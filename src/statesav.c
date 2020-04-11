atari800/atari800

commit fd5499cb9f4c91bc36a04273ebba12d47bdf59c6

Fix warning when !HAVE_LIBZ

src/statesav.c
@@ -37,9 +37,6 @@
 #endif
 #ifdef HAVE_LIBZ
 #include <zlib.h>
-#else
-#define gzFile char *
-#define Z_OK 0
 #endif
 #ifdef LIBATARI800
 #include "libatari800/init.h"

commit 546e8da811dc892d4f34b9820c61122a768a6fdc

Correct build error
Introduced by fd5499cb, libatari800 requires special treatment.

Fixes #95.

src/statesav.c
@@ -71,6 +71,11 @@
 #define SAVE_VERSION_NUMBER 8 /* Last changed after Atari800 3.1.0 */
 
 #if defined(MEMCOMPR) || defined(LIBATARI800)
+/* libatari800 pretends to care about libz but it doesn't */
+#ifdef LIBATARI800
+#define gzFile char *
+#define Z_OK 0
+#endif
 static gzFile mem_open(const char *name, const char *mode);
 static int mem_close(gzFile stream);
 static size_t mem_read(void *buf, size_t len, gzFile stream);