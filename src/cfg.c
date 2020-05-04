027dd3f8652152303768df6c9fe4f2d0151773de
Walt Drummond <walt@drummond.us>
2020-05-04 10:02:16 -0700

    Look for comment markers beyond the first column

--- src/cfg.c
+++ src/cfg.c
@@ -126,6 +126,7 @@ int CFG_LoadConfig(const char *alternate_config_filename)
 	while (fgets(string, sizeof(string), fp)) {
 		char *ptr;
 		Util_chomp(string);
+		Util_trim(string);
 		/* Check for comments */
 		if (string != NULL && string[0] == '#') {
 			continue;
