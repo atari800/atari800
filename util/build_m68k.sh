bf387d453e3c22b87d10cc78e0dbf35cba24dcce
Miro Kropacek <miro.kropacek@gmail.com>
2022-10-17 22:12:37 +0200

    Disable audio/video recording for Falcon

--- util/build_m68k.sh
+++ util/build_m68k.sh
@@ -9,6 +9,9 @@ DIR_ATARI800_CF="atari800-cf"
 
 DIR_SYSROOT="$(m68k-atari-mint-gcc -print-sysroot)"
 
+COMMON_FLAGS='--host=m68k-atari-mint --enable-veryslow --disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-eventrecording
+	--disable-pokeyrec --disable-videorecording --disable-monitorutf8 --disable-monitoransi --disable-pbi_bb --disable-pbi_mio --disable-riodevice --with-readline=no'
+
 rm -rf ${DIR_ATARI800_CLASSIC} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
 mkdir -p ${DIR_ATARI800_CLASSIC} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
 
@@ -18,27 +21,19 @@ git clean -f -x -d -f
 cd -
 
 cd ${DIR_ATARI800_CLASSIC}
-${DIR_ATARI800_SRC}/configure --host=m68k-atari-mint --enable-veryslow --disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-eventrecording --disable-monitorutf8 --disable-monitoransi --disable-pbi_bb --disable-pbi_mio --disable-riodevice --with-readline=no \
-	--target=falcon --enable-falconcpuasm \
-	&& make V=1
+${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm && make V=1
 cd -
 
 cd ${DIR_ATARI800_FAST}
-${DIR_ATARI800_SRC}/configure --host=m68k-atari-mint --enable-veryslow --disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-eventrecording --disable-monitorutf8 --disable-monitoransi --disable-pbi_bb --disable-pbi_mio --disable-riodevice --with-readline=no \
-	--target=falcon --enable-falconcpuasm --disable-newcycleexact \
-	&& make V=1
+${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=falcon --enable-falconcpuasm --disable-newcycleexact && make V=1
 cd -
 
 cd ${DIR_ATARI800_SDL}
-PATH=${DIR_SYSROOT}/usr/bin/m68020-60:$PATH ${DIR_ATARI800_SRC}/configure --host=m68k-atari-mint --enable-veryslow --disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-eventrecording --disable-monitorutf8 --disable-monitoransi --disable-pbi_bb --disable-pbi_mio --disable-riodevice --with-readline=no \
-	--target=default --enable-falconcpuasm \
-	&& make V=1
+PATH=${DIR_SYSROOT}/usr/bin/m68020-60:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=default --enable-falconcpuasm && make V=1
 cd -
 
 cd ${DIR_ATARI800_CF}
-PATH=${DIR_SYSROOT}/usr/bin/m5475:$PATH ${DIR_ATARI800_SRC}/configure --host=m68k-atari-mint --enable-veryslow --disable-monitorbreak --disable-monitorhints --disable-crashmenu --disable-monitorasm --disable-eventrecording --disable-monitorutf8 --disable-monitoransi --disable-pbi_bb --disable-pbi_mio --disable-riodevice --with-readline=no \
-	--target=firebee --enable-pagedattrib --enable-cyclesperopcode \
-	&& make V=1
+PATH=${DIR_SYSROOT}/usr/bin/m5475:$PATH ${DIR_ATARI800_SRC}/configure $COMMON_FLAGS --target=firebee --enable-pagedattrib --enable-cyclesperopcode && make V=1
 cd -
 
 for d in ${DIR_ATARI800_CLASSIC} ${DIR_ATARI800_FAST} ${DIR_ATARI800_SDL} ${DIR_ATARI800_CF}
