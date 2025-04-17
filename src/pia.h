5c51ed3b9aec0748d183faf2f26c2d6ce1331667
mozzwald <mozzwald@gmail.com>
2025-04-17 12:24:27 -0500

    pia: add SIO PROCEED and INTERRUPT registers

--- src/pia.h
+++ src/pia.h
@@ -15,6 +15,9 @@ extern UBYTE PIA_PORTB;
 extern UBYTE PIA_PORTA_mask;
 extern UBYTE PIA_PORTB_mask;
 extern UBYTE PIA_PORT_input[2];
+/* PROCEED/INTERRUPT pin support (CA1/CB1) */
+extern int PIA_CA1;
+extern int PIA_CB1;
 extern int PIA_CA2;
 extern int PIA_CB2;
 extern int PIA_IRQ;
@@ -25,5 +28,8 @@ UBYTE PIA_GetByte(UWORD addr, int no_side_effects);
 void PIA_PutByte(UWORD addr, UBYTE byte);
 void PIA_StateSave(void);
 void PIA_StateRead(UBYTE version);
+/* Set PROCEED (CA1) and INTERRUPT (CB1) pin values */
+void PIA_SetCA1(int value);
+void PIA_SetCB1(int value);
 
 #endif /* PIA_H_ */
