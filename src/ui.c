atari800/atari800

commit 6a2827ca8446c1ee7335e93ba3ef50f7bae4f149

moving "Save configuration" menu entry to the root menu

src/ui.c
@@ -2270,8 +2270,7 @@ static void AtariSettings(void)
 		UI_MENU_SUBMENU(13, "System ROM settings"),
 		UI_MENU_SUBMENU(14, "Configure directories"),
 #ifndef DREAMCAST
-		UI_MENU_ACTION(15, "Save configuration file"),
-		UI_MENU_CHECK(16, "Save configuration on exit:"),
+		UI_MENU_CHECK(16, "Auto-save configuration on exit:"),
 #endif
 		UI_MENU_END
 	};
@@ -2341,9 +2340,6 @@ static void AtariSettings(void)
 			ConfigureDirectories();
 			break;
 #ifndef DREAMCAST
-		case 15:
-			UI_driver->fMessage(CFG_WriteConfig() ? "Configuration file updated" : "Error writing configuration file", 1);
-			break;
 		case 16:
 			CFG_save_on_exit = !CFG_save_on_exit;
 			break;
@@ -4367,14 +4363,12 @@ void UI_Run(void)
 		UI_MENU_FILESEL_ACCEL(UI_MENU_LOADSTATE, "Load State", "Alt+L"),
 #if SCREENSHOTS
 #ifdef HAVE_LIBPNG
-		UI_MENU_FILESEL_ACCEL(UI_MENU_PCX, "Save Screenshot", "F10"),
-		/* there isn't enough space for "PNG/PCX Interlaced Screenshot Shift+F10" */
-		UI_MENU_FILESEL_ACCEL(UI_MENU_PCXI, "Save Interlaced Screenshot", "Shift+F10"),
+		UI_MENU_FILESEL_ACCEL(UI_MENU_PCX, "Screenshot (+Shift = interlaced)", "F10"),
 #else
-		UI_MENU_FILESEL_ACCEL(UI_MENU_PCX, "PCX Screenshot", "F10"),
-		UI_MENU_FILESEL_ACCEL(UI_MENU_PCXI, "PCX Interlaced Screenshot", "Shift+F10"),
+		UI_MENU_FILESEL_ACCEL(UI_MENU_PCX, "PCX Screenshot (+Shift = interlaced)", "F10"),
 #endif
 #endif
+		UI_MENU_ACTION(UI_MENU_SAVE_CONFIG, "Save Configuration"),
 		UI_MENU_ACTION_ACCEL(UI_MENU_BACK, "Back to Emulated Atari", "Esc"),
 		UI_MENU_ACTION_ACCEL(UI_MENU_RESETW, "Reset (Warm Start)", "F5"),
 		UI_MENU_ACTION_ACCEL(UI_MENU_RESETC, "Reboot (Cold Start)", "Shift+F5"),
@@ -4487,6 +4481,9 @@ void UI_Run(void)
 			break;
 		case UI_MENU_PCXI:
 			Screenshot(TRUE);
+			break;
+		case UI_MENU_SAVE_CONFIG:
+			UI_driver->fMessage(CFG_WriteConfig() ? "Configuration file updated" : "Error writing configuration file", 1);
 			break;
 #endif
 #endif

src/ui.h
@@ -84,7 +84,7 @@ int GetRawKey(void);
 #define UI_MENU_WINDOWS	         20
 #define UI_MENU_QUICKSAVESTATE   21
 #define UI_MENU_QUICKLOADSTATE   22
-
+#define UI_MENU_SAVE_CONFIG      23
 
 #define UI_MENU_VIDEO_RECORDING  24
 

commit aec88fbdb1c1e4e8e963e71093951068abdf637b

UI: Fix "Save Configuration"
Regression from commit 6a2827ca: when SCREENSHOTS were not defined, the
menu entry would do nothing. Fix also the DREAMCAST regression for the
sake of completeness.

src/ui.c
@@ -4362,7 +4362,9 @@ void UI_Run(void)
 		UI_MENU_FILESEL_ACCEL(UI_MENU_PCX, "PCX Screenshot (+Shift = interlaced)", "F10"),
 #endif
 #endif
+#ifndef DREAMCAST
 		UI_MENU_ACTION(UI_MENU_SAVE_CONFIG, "Save Configuration"),
+#endif
 		UI_MENU_ACTION_ACCEL(UI_MENU_BACK, "Back to Emulated Atari", "Esc"),
 		UI_MENU_ACTION_ACCEL(UI_MENU_RESETW, "Reset (Warm Start)", "F5"),
 		UI_MENU_ACTION_ACCEL(UI_MENU_RESETC, "Reboot (Cold Start)", "Shift+F5"),
@@ -4476,10 +4478,12 @@ void UI_Run(void)
 		case UI_MENU_PCXI:
 			Screenshot(TRUE);
 			break;
+#endif
+#endif
+#ifndef DREAMCAST
 		case UI_MENU_SAVE_CONFIG:
 			UI_driver->fMessage(CFG_WriteConfig() ? "Configuration file updated" : "Error writing configuration file", 1);
 			break;
-#endif
 #endif
 #ifndef USE_CURSES
 		case UI_MENU_CONTROLLER: